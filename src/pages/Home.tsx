440476eaa3b61b35668f96d3f262b5aaab1adbed
juvenal <juanjuvenaldo@gmail.com>
2023-08-07 16:26:28 -0500

    Add home page.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,13 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonButton,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Home.css";
 
 const Home: React.FC = () => {
   return (
@@ -13,10 +20,17 @@ const Home: React.FC = () => {
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">Welcome</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        <IonContent class="ion-padding">
+          <h1>¡Hola!</h1>
+          <p>¿Estás listo para anticipar tus rentas?</p>
+          <IonButton>Crear cuenta</IonButton>
+          <div>
+            <a>Términos y condiciones</a>
+          </div>
+        </IonContent>
       </IonContent>
     </IonPage>
   );
