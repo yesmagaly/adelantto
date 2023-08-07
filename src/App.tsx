7052b8b006ed2879a132c8a900a3de2b9ce59222
juvenal <juanjuvenaldo@gmail.com>
2023-08-07 18:15:53 -0500

    Added a new route for the email verification page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { IonReactRouter } from "@ionic/react-router";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import VerificationCode from "./pages/VerificationCode";
+import VerificationEmail from "./pages/VerificationEmail";
 
 /* Core CSS required for Ionic components to work properly */
 import "@ionic/react/css/core.css";
@@ -38,6 +39,9 @@ const App: React.FC = () => (
         </Route>
         <Route exact path="/verification-code">
           <VerificationCode />
+        </Route>
+        <Route exact path="/verification-email">
+          <VerificationEmail />
         </Route>
         <Route exact path="/">
           <Redirect to="/home" />
