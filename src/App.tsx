12da3e154b10f1e3bc85d07b0a52d44b379c76f3
juvenal <juanjuvenaldo@gmail.com>
2023-08-07 17:56:40 -0500

    Added a new route for the code verification page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { IonApp, IonRouterOutlet, setupIonicReact } from "@ionic/react";
 import { IonReactRouter } from "@ionic/react-router";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
+import VerificationCode from "./pages/VerificationCode";
 
 /* Core CSS required for Ionic components to work properly */
 import "@ionic/react/css/core.css";
@@ -34,6 +35,9 @@ const App: React.FC = () => (
         </Route>
         <Route exact path="/register">
           <Register />
+        </Route>
+        <Route exact path="/verification-code">
+          <VerificationCode />
         </Route>
         <Route exact path="/">
           <Redirect to="/home" />
