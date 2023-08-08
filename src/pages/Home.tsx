556fcd79abf3872f6299c572c4033decac56b130
yesmagaly <yesmagaly@gmail.com>
2023-08-07 20:33:45 -0500

    Refactor Adelantto home page.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,28 +8,27 @@ import {
 } from "@ionic/react";
 import "./Home.css";
 
+import logo from "../assets/icons/logo.svg";
+
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Welcome</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent class="ion-padding">
-          <h1>¡Hola!</h1>
-          <p>¿Estás listo para anticipar tus rentas?</p>
-          <IonButton>Crear cuenta</IonButton>
-          <div>
-            <a>Términos y condiciones</a>
-          </div>
-        </IonContent>
+        <div className="flex items-center flex-col justify-center text-center h-full">
+          <img className="mb-24" src={logo} />
+          <h1 className="font-semibold text-[40px]">¡Hola!</h1>
+          <p className="w-44 mb-14 leading-5">
+            ¿Estás listo para anticipar tus rentas?
+          </p>
+          <button className="font-semibold mb-4 bg-primary-green px-7 py-3 rounded text-white">
+            Crear cuenta
+          </button>
+          <p>
+            <a href="#" className="text-sm">
+              Términos y condiciones
+            </a>
+          </p>
+        </div>
       </IonContent>
     </IonPage>
   );
