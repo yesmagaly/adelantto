fe8b7b2b0ed082786dbe3ae5191919710fdaa853
yesmagaly <yesmagaly@gmail.com>
2023-08-08 11:50:22 -0500

    Add styles in the AdvanceImmediately and CreateProfile pages.

--- src/pages/AdvanceImmediately.tsx
+++ src/pages/AdvanceImmediately.tsx
@@ -13,28 +13,27 @@ import { logoIonic, ellipse } from "ionicons/icons";
 const AdvanceImmediately: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar></IonToolbar>
-        </IonHeader>
-        <IonContent class="ion-padding">
-          <span>Paso 1 de 3</span>
-          <h1>
+        <div className="h-60 px-9 py-6">
+          <div className="heading__pager text-right">Paso 1 de 3</div>
+          <h1 className="heading__title text-center pt-20">
             <strong>Adelanto</strong> <br /> de inmediato
           </h1>
-          <p>
-            Recibe un adelanto de tus rentas <br />
-            <strong>en tan solo 72 horas</strong>
-          </p>
-          <IonIcon icon={ellipse}></IonIcon>
-          <IonIcon icon={ellipse}></IonIcon>
-          <IonIcon icon={ellipse}></IonIcon>
-        </IonContent>
+        </div>
+
+        <div className="content">
+          <div className="mb-14">
+            <p className="help-text">
+              Recibe un adelanto de tus rentas <br />
+              <strong className="text-xl">en tan solo 72 horas.</strong>
+            </p>
+          </div>
+          <div>
+            <IonIcon icon={ellipse}></IonIcon>
+            <IonIcon icon={ellipse}></IonIcon>
+            <IonIcon icon={ellipse}></IonIcon>
+          </div>
+        </div>
       </IonContent>
     </IonPage>
   );
