e0cb8a747de7b625b13d2bac0b377c8c4271548e
juvenal <juanjuvenaldo@gmail.com>
2023-08-14 15:37:46 -0500

    Added page search .

---
+++ src/pages/Search.tsx
@@ -0,0 +1,31 @@
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonButton,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+} from "@ionic/react";
+import "./Home.css";
+
+const Search: React.FC = () => {
+  return (
+    <IonPage>
+      <IonContent fullscreen>
+        <IonCard>
+          <img
+            alt="Silhouette of mountains"
+            src="https://ionicframework.com/docs/img/demos/card-media.png"
+          />
+          <IonCardHeader>
+            <IonCardTitle>Buscar</IonCardTitle>
+          </IonCardHeader>
+          <IonButton>Continuar</IonButton>
+        </IonCard>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Search;
