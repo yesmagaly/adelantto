99f766dfc7aa68009b5f419e73ae31930084cc09
juvenal <juanjuvenaldo@gmail.com>
2023-08-17 12:27:27 -0500

    Added page outlay .

---
+++ src/pages/Outlay.tsx
@@ -0,0 +1,55 @@
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonButton,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonAvatar,
+  IonCheckbox,
+  IonList,
+  IonItem,
+  IonLabel,
+} from "@ionic/react";
+import "./Home.css";
+
+const Outlay: React.FC = () => {
+  return (
+    <IonPage>
+      <IonContent fullscreen>
+        <div>
+          <strong>Desembolso</strong>
+          <p>Conoce el desglose de tu servicio</p>
+        </div>
+        <IonList>
+          <IonItem>
+            <IonLabel>
+              <strong>térmio de desembolso</strong>
+            </IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>
+              <strong>Tiempos interbancarios</strong>
+            </IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>
+              <strong>Claridad en comisiones</strong>{" "}
+            </IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>
+              <strong>
+                Envío de notificación con <br /> tracking
+              </strong>
+            </IonLabel>
+          </IonItem>
+        </IonList>
+        <IonButton>Siguiente</IonButton>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Outlay;
