e163f729a76316cc1e92370b7dfb05ab75591056
yesmagaly <yesmagaly@gmail.com>
2023-08-18 22:55:48 -0500

    Refactor UploadDocuments page.

--- src/pages/UploadDocuments.tsx
+++ src/pages/UploadDocuments.tsx
@@ -1,72 +1,81 @@
-import {
-  IonContent,
-  IonHeader,
-  IonPage,
-  IonTitle,
-  IonToolbar,
-  IonButton,
-  IonIcon,
-  IonList,
-  IonItem,
-  IonLabel,
-} from "@ionic/react";
+import { IonContent, IonPage, IonButton, IonList } from "@ionic/react";
 import "./Home.css";
+
+import FileInputItem from "../components/FileInputItem";
 
 const UploadDocuments: React.FC = () => {
   return (
     <IonPage>
       <IonContent fullscreen>
-        <div>
-          <div>Paso 3 de 3</div>
-          <strong>
-            A continuación <br /> sube los siguientes documentos <br /> para tu
-            propiedad
-          </strong>
+        <div className="heading-blue">
+          <div className="heading__pager text-right">Paso 3 de 3</div>
+          <h4 className="text-xl">
+            A continuación <br />
+            <strong>
+              sube los siguientes documentos <br />
+            </strong>
+            para tu propiedad
+          </h4>
         </div>
-        <IonList>
-          <IonItem>
-            <IonLabel>
-              Caratula de tu escritura <br /> Con sello de inscripsión del
-              Registro Público <br /> de la Propiedad{" "}
-            </IonLabel>
-          </IonItem>
-          <IonItem>
-            <IonLabel>
-              Contrato de arrendamiento <br /> Firmado por ambas partes{" "}
-            </IonLabel>
-          </IonItem>
-          <IonItem>
-            <IonLabel>Copia del último pago predial del inmueble </IonLabel>
-          </IonItem>
-          <IonItem>
-            <IonLabel>
+        <div className="px-5 py-6">
+          <FileInputItem>
+            <h5 className="font-bold text-xs leading-3">
+              Caratula de tu escritura
+            </h5>
+            <p className="text-[10px]">
+              Con sello de inscripsión del Registro Público de la Propiedad
+            </p>
+          </FileInputItem>
+
+          <FileInputItem>
+            <h5 className="font-bold text-xs leading-3">
+              Contrato de arrendamiento
+            </h5>
+            <p className="text-[10px]">Firmado por ambas partes</p>
+          </FileInputItem>
+
+          <FileInputItem>
+            <h5 className="font-bold text-xs leading-3">
+              Copia del último pago predial del inmueble
+            </h5>
+          </FileInputItem>
+
+          <FileInputItem>
+            <h5 className="font-bold text-xs leading-3">
               Comprobante de ingresos
-              <br /> útimos tres meses (Nóminas o Bancarios){" "}
-            </IonLabel>
-          </IonItem>
-          <IonItem>
-            <IonLabel>
+            </h5>
+            <p className="text-[10px]">
+              útimos tres meses (Nóminas o Bancarios)
+            </p>
+          </FileInputItem>
+
+          <FileInputItem>
+            <h5 className="font-bold text-xs leading-3">
               Certificado de fiscalización en el RPP
-              <br /> Registro Público de la Propiedad{" "}
-            </IonLabel>
-          </IonItem>
-          <IonItem>
-            <IonLabel>
-              RFC <br /> Constancia de situación fiscal con antigüedad <br /> no
-              mayor a 3 meses{" "}
-            </IonLabel>
-          </IonItem>
-          <IonItem>
-            <IonLabel>
-              CURP <br /> Certificada y emitida por la RENAPO{" "}
-            </IonLabel>
-          </IonItem>
-        </IonList>
-        <p>
-          Los documentos deberán ser escaneados en alta resolución <br /> y en
-          formato PDF, de lo contrario declinaremos el proceso.
-        </p>
-        <IonButton>Siguiente</IonButton>
+            </h5>
+            <p className="text-[10px]">Registro Público de la Propiedad</p>
+          </FileInputItem>
+
+          <FileInputItem>
+            <h5 className="font-bold text-xs leading-3">RFC</h5>
+            <p className="text-[10px]">
+              Constancia de situación fiscal con antigüedad no mayor a 3 meses
+            </p>
+          </FileInputItem>
+
+          <FileInputItem>
+            <h5 className="font-bold text-xs leading-3">CURP</h5>
+            <p className="text-[10px]">Certificada y emitida por la RENAPO</p>
+          </FileInputItem>
+        </div>
+
+        <div className="text-center">
+          <p className="text-xs mb-4">
+            Los documentos deberán ser escaneados en alta resolución <br /> y en
+            formato PDF, de lo contrario declinaremos el proceso.
+          </p>
+          <button className="bg-green-600 py-1 px-8 rounded">Siguiente</button>
+        </div>
       </IonContent>
     </IonPage>
   );
