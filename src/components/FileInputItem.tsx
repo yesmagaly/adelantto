86c9b48e107da3531a3a6f4a3c545dcb753797d2
yesmagaly <yesmagaly@gmail.com>
2023-08-18 21:55:53 -0500

    Add a new FileInputItem component.

---
+++ src/components/FileInputItem.tsx
@@ -0,0 +1,23 @@
+import uploadIcon from "../assets/icons/upload.svg";
+
+interface ComponentProp {
+  children: any;
+}
+
+const UploadDocuments: React.FC<ComponentProp> = ({ children }) => {
+  return (
+    <div className="px-5 py-6">
+      <div className="flex gap-4">
+        <div className="flex justify-center items-center basis-28 border-b border-r border-gray-500 border-solid">
+          <div className="flex gap-5">
+            <input type="checkbox" id="upload" />
+            <img src={uploadIcon} />
+          </div>
+        </div>
+        <div>{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default UploadDocuments;
