b2f5f9dcd65be51b73dc4d5a1bdec925264b77f0
Yesica Ramirez <yesmagaly@gmail.com>
2023-08-19 19:35:48 -0500

    Update styles on the FileInputItem component.

--- src/components/FileInputItem.tsx
+++ src/components/FileInputItem.tsx
@@ -8,13 +8,13 @@ const UploadDocuments: React.FC<ComponentProp> = ({ children }) => {
   return (
     <div className="px-5">
       <div className="flex gap-4">
-        <div className="flex justify-center items-center basis-28 border-b border-r border-gray-500 border-solid">
+        <div className="flex justify-center items-center basis-32 border-b border-r-2 border-[#D8D8D8] border-solid">
           <div className="flex gap-5">
             <input type="checkbox" id="upload" />
             <img src={uploadIcon} />
           </div>
         </div>
-        <div className="my-4">{children}</div>
+        <div className="my-4 basis-full leading-3">{children}</div>
       </div>
     </div>
   );
