f0ee2a47d3e78e18e5a8a6618b53584dde28df22
Yesica Ramirez <yesmagaly@gmail.com>
2023-08-23 18:06:33 -0500

    Refactor styles on the verification email page.

--- src/pages/VerificationEmail.tsx
+++ src/pages/VerificationEmail.tsx
@@ -1,4 +1,7 @@
 import { IonContent, IonPage, useIonRouter } from "@ionic/react";
+import Lottie from "react-lottie-player";
+
+import emailAnimation from "../assets/animations/email.json"
 
 const VerificationEmail: React.FC = () => {
   const router = useIonRouter();
@@ -16,10 +19,18 @@ const VerificationEmail: React.FC = () => {
         </div>
 
         <div className="content">
-          <form className="form">
+          <Lottie
+            animationData={emailAnimation}
+            style={{ width: 174, height: 262 }}
+            loop
+            play
+          />
+
+          <form className="form mb-9">
             <input type="email" placeholder="Email" />
           </form>
-          <div className="mb-32">
+
+          <div className="mb-24">
             <p className="help-text mb-4">
               Enviaremos una contraseña tu cuenta de correo para que puedas{" "}
               <a className="underline">iniciar sesión.</a>
@@ -27,11 +38,13 @@ const VerificationEmail: React.FC = () => {
           </div>
 
           <button
-            className="button-primary"
+            className="button-primary mb-16"
             onClick={() => router.push("/advance-immediately")}
           >
             Enviar código
           </button>
+
+          <div className="border-bottom" />
         </div>
       </IonContent>
     </IonPage>
