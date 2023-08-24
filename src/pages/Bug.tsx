09ff96f903b285856f88e6a6152d933fb1befa69
Yesica Ramirez <yesmagaly@gmail.com>
2023-08-23 19:34:07 -0500

    Refactor bug page.

--- src/pages/Bug.tsx
+++ src/pages/Bug.tsx
@@ -1,23 +1,39 @@
-import {
-  IonContent,
-  IonHeader,
-  IonPage,
-  IonButton,
-  IonCard,
-  IonCardHeader,
-  IonCardTitle,
-} from "@ionic/react";
+import { IonContent, IonPage, useIonRouter } from "@ionic/react";
+import Lottie from "react-lottie-player";
+
+import wrongCodeAnimation from "../assets/animations/wrong-code.json"
 
 const Bug: React.FC = () => {
+  const router = useIonRouter();
+
   return (
     <IonPage>
       <IonContent fullscreen>
-        <div>
-          <strong>¡Ups!</strong>
-        </div>
-        <p>El código que ingresaste es incorrecto, inténtalo nuevamente.</p>
+        <div className="heading-light-green flex p-4 flex-col items-center text-center mb-10">
+          <h1 className="font-bold text-4xl text-center">¡Ups!</h1>
 
-        <IonButton>Enviar código</IonButton>
+          <div className="flex items-center">
+            <Lottie
+              animationData={wrongCodeAnimation}
+              style={{ width: 174, height: 262 }}
+              loop
+              play
+            />
+          </div>
+        </div>
+
+        <div className="content">
+          <p className="help-text border-y border-solid border-primary-blue py-6 mb-28">El código que ingresaste es incorrecto, inténtalo nuevamente.</p>
+
+          <button
+            className="button-primary mb-16"
+            onClick={() => router.push("/register")}
+          >
+            Enviar código
+          </button>
+
+          <div className="border-bottom" />
+        </div>
       </IonContent>
     </IonPage>
   );
