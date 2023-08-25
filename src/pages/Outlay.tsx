ca04b1f042cb51bf9c38b6e273b1a13c64178a4c
juvenal <juanjuvenaldo@gmail.com>
2023-08-25 17:49:01 -0500

    Added on page four modal .

--- src/pages/Outlay.tsx
+++ src/pages/Outlay.tsx
@@ -1,51 +1,169 @@
 import {
   IonContent,
-  IonHeader,
   IonPage,
-  IonButton,
-  IonCard,
-  IonCardHeader,
-  IonCardTitle,
-  IonAvatar,
-  IonCheckbox,
   IonList,
   IonItem,
   IonLabel,
+  useIonRouter,
 } from "@ionic/react";
 
 const Outlay: React.FC = () => {
+  const router = useIonRouter();
   return (
     <IonPage>
       <IonContent fullscreen>
         <div>
-          <strong>Desembolso</strong>
+          <h3>
+            <strong>Desembolso</strong>
+          </h3>
           <p>Conoce el desglose de tu servicio</p>
         </div>
         <IonList>
           <IonItem>
             <IonLabel>
-              <strong>térmio de desembolso</strong>
+              <p>
+                <strong>términos</strong> de desembolso
+              </p>
             </IonLabel>
           </IonItem>
           <IonItem>
             <IonLabel>
-              <strong>Tiempos interbancarios</strong>
+              <p>
+                <strong>Tiempos </strong> interbancarios
+              </p>
             </IonLabel>
           </IonItem>
           <IonItem>
             <IonLabel>
-              <strong>Claridad en comisiones</strong>{" "}
+              <p>
+                <strong>Claridad </strong> en comisiones
+              </p>
             </IonLabel>
           </IonItem>
           <IonItem>
             <IonLabel>
-              <strong>
-                Envío de notificación con <br /> tracking
-              </strong>
+              <p>
+                <strong>Envío de notificación </strong> con <br /> tracking
+              </p>
             </IonLabel>
           </IonItem>
         </IonList>
-        <IonButton>Siguiente</IonButton>
+        <button
+          className="button-primary mb-16"
+          onClick={() => router.push("")}
+        >
+          Siguiente
+        </button>
+        <div className="border-bottom" />
+
+        <div>
+          <h3>
+            <strong>
+              Términos <br /> de desembolso
+            </strong>
+          </h3>
+          <h6>
+            <strong>
+              1. Lorem ipsum dolor sit amet,
+              <br /> consectetuer adipiscing elit, sed
+            </strong>
+          </h6>
+          <p>
+            diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam
+            erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci
+            tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
+            consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate
+            velit esse molestie consequat, vel illum dolore eu feugiat nulla
+            facilisis at vero eros et accumsan et iusto odio dignissim qui
+            blandit praesent luptatum zzril delenit augue duis dolore te feugait
+            nulla facilisi. Lorem ipsum dolor sit amet, cons ectetuer adipiscing
+            elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+            magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
+            nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip
+            ex ea commodo consequat.
+          </p>
+        </div>
+        <div>
+          <h3>
+            <strong>
+              Tiempos <br /> interbancarios
+            </strong>
+          </h3>
+          <h6>
+            <strong>
+              1. Lorem ipsum dolor sit amet,
+              <br /> consectetuer adipiscing elit, sed
+            </strong>
+          </h6>
+          <p>
+            diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam
+            erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci
+            tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
+            consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate
+            velit esse molestie consequat, vel illum dolore eu feugiat nulla
+            facilisis at vero eros et accumsan et iusto odio dignissim qui
+            blandit praesent luptatum zzril delenit augue duis dolore te feugait
+            nulla facilisi. Lorem ipsum dolor sit amet, cons ectetuer adipiscing
+            elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+            magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
+            nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip
+            ex ea commodo consequat.
+          </p>
+        </div>
+        <div>
+          <h3>
+            <strong>
+              Claridad en <br /> comisiones
+            </strong>
+          </h3>
+          <h6>
+            <strong>
+              1. Lorem ipsum dolor sit amet,
+              <br /> consectetuer adipiscing elit, sed
+            </strong>
+          </h6>
+          <p>
+            diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam
+            erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci
+            tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
+            consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate
+            velit esse molestie consequat, vel illum dolore eu feugiat nulla
+            facilisis at vero eros et accumsan et iusto odio dignissim qui
+            blandit praesent luptatum zzril delenit augue duis dolore te feugait
+            nulla facilisi. Lorem ipsum dolor sit amet, cons ectetuer adipiscing
+            elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+            magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
+            nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip
+            ex ea commodo consequat.
+          </p>
+        </div>
+        <div>
+          <h3>
+            <strong>
+              Envío de <br /> notificación <br /> con tracking
+            </strong>
+          </h3>
+          <h6>
+            <strong>
+              1. Lorem ipsum dolor sit amet,
+              <br /> consectetuer adipiscing elit, sed
+            </strong>
+          </h6>
+          <p>
+            diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam
+            erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci
+            tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
+            consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate
+            velit esse molestie consequat, vel illum dolore eu feugiat nulla
+            facilisis at vero eros et accumsan et iusto odio dignissim qui
+            blandit praesent luptatum zzril delenit augue duis dolore te feugait
+            nulla facilisi. Lorem ipsum dolor sit amet, cons ectetuer adipiscing
+            elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+            magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
+            nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip
+            ex ea commodo consequat.
+          </p>
+        </div>
       </IonContent>
     </IonPage>
   );
