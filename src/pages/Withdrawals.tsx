3b8ce54f0b328d6ab7c013409de03abca909db8c
juvenal <juanjuvenaldo@gmail.com>
2023-08-25 11:34:22 -0500

    Added the style button and modal interbank terms

--- src/pages/Withdrawals.tsx
+++ src/pages/Withdrawals.tsx
@@ -1,24 +1,15 @@
-import {
-  IonContent,
-  IonHeader,
-  IonPage,
-  IonButton,
-  IonCard,
-  IonCardHeader,
-  IonCardTitle,
-  IonAvatar,
-  IonCheckbox,
-} from "@ionic/react";
+import { IonContent, IonPage, useIonRouter } from "@ionic/react";
 
 const Withdrawals: React.FC = () => {
+  const router = useIonRouter();
   return (
     <IonPage>
       <IonContent fullscreen>
         <div>
-          <strong>
+          <h3>
             ¿Dónde quieres <br />
-            tu dinero?
-          </strong>
+            <strong>tu dinero?</strong>
+          </h3>
         </div>
         <div>
           <form>
@@ -28,12 +19,45 @@ const Withdrawals: React.FC = () => {
             />
           </form>
         </div>
-        <a href="">Términos interbancarios</a>
+        <a className="underline">Términos interbancarios</a>
         <p>
           A continuación encontrarás el contrato <br /> relacionado a tu
           solicitud.
         </p>
-        <IonButton>Siguiente</IonButton>
+        <button
+          className="button-primary mb-16"
+          onClick={() => router.push("/verification-email")}
+        >
+          Siguiente
+        </button>
+        <div className="border-bottom" />
+        <div>
+          <h2>
+            <strong>
+              Términos <br /> interbancarios
+            </strong>
+          </h2>
+          <h5>
+            <strong>
+              1. Lorem ipsum dolor sit amet,
+              <br /> consectetuer adipiscing elit, sed
+            </strong>
+          </h5>
+          <p>
+            diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam
+            erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci
+            tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
+            consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate
+            velit esse molestie consequat, vel illum dolore eu feugiat nulla
+            facilisis at vero eros et accumsan et iusto odio dignissim qui
+            blandit praesent luptatum zzril delenit augue duis dolore te feugait
+            nulla facilisi. Lorem ipsum dolor sit amet, cons ectetuer adipiscing
+            elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+            magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
+            nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip
+            ex ea commodo consequat.
+          </p>
+        </div>
       </IonContent>
     </IonPage>
   );
