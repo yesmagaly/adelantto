c8e37f81d75697dcf8877c7db4fbfc8a34f30215
juvenal <juanjuvenaldo@gmail.com>
2023-08-24 21:10:02 -0500

    Modified the style of button .

--- src/pages/ValidationError.tsx
+++ src/pages/ValidationError.tsx
@@ -1,27 +1,26 @@
-import {
-  IonContent,
-  IonHeader,
-  IonPage,
-  IonButton,
-  IonCard,
-  IonCardHeader,
-  IonCardTitle,
-  IonAvatar,
-  IonCheckbox,
-} from "@ionic/react";
+import { IonContent, useIonRouter, IonPage } from "@ionic/react";
 
 const ValidationError: React.FC = () => {
+  const router = useIonRouter();
   return (
     <IonPage>
       <IonContent fullscreen>
         <div>
-          <strong>¡Lo sentimos!</strong>
+          <h3>
+            <strong>¡Lo sentimos!</strong>
+          </h3>
         </div>
         <p>
           Los datos de tu propiedad no han pasado <br /> nuestro proceso de
           validación
         </p>
-        <IonButton>Finalizar</IonButton>
+        <button
+          className="button-primary mb-16"
+          onClick={() => router.push("")}
+        >
+          Finalizar
+        </button>
+        <div className="border-bottom" />
       </IonContent>
     </IonPage>
   );
