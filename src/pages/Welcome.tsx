b8fd6a080438a177345fc4ce30d63ff9b3e4d977
yesmagaly <yesmagaly@gmail.com>
2023-08-26 12:13:38 -0500

    Refactor styles on the welcome page.

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,30 +1,31 @@
-import {
-  IonContent,
-  IonHeader,
-  IonPage,
-  IonButton,
-  IonCard,
-  IonCardHeader,
-  IonCardTitle,
-} from "@ionic/react";
+import { IonContent, IonPage, IonButton } from "@ionic/react";
+import logo from "../assets/icons/alternative-logo.svg";
 
 const Welcome: React.FC = () => {
   return (
     <IonPage>
       <IonContent fullscreen>
-        <h1>¡Bienvenido!</h1>
-        <div>
-          <strong>
-            Ahora eres parte <br /> de Adelantto
-          </strong>
+        <div className="body__blue-bg text-white flex items-center flex-col justify-center text-center h-full">
+          <img className="mb-5" src={logo} />
+          <h1 className="font-semibold text-4xl mb-5">¡Bienvenido!</h1>
+          <p className="text-xl leading-6 mb-12">
+            Ahora eres parte <br />
+            <strong>de Adelantto</strong>
+          </p>
+
+          <form className="form mb-4">
+            <h5 className="mb-5">Actualiza tu contraseña</h5>
+            <input type="text" placeholder="Nueva contraseña" />
+            <input type="text" placeholder="Repite tu nueva contraseña" />
+          </form>
+          <a
+            className="font-semibold text-primary-green text-sm underline mb-10"
+            href=""
+          >
+            Validación de password*
+          </a>
+          <IonButton>Cambiar contraseña</IonButton>
         </div>
-        <p>Actualiza tu contraseña</p>
-        <form action="">
-          <input type="text" placeholder="Nueva contraseña" />
-          <input type="text" placeholder="Repite tu nueva contraseña" />
-        </form>
-        <a href="">Validación de password*</a>
-        <IonButton>Cambiar contraseña</IonButton>
       </IonContent>
     </IonPage>
   );
