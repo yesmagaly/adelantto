a8dc9658728e8c5c49d18a690a4963db26c0518c
juvenal <juanjuvenaldo@gmail.com>
2023-08-28 17:26:59 -0500

    Added style to content .

--- src/pages/CorrectData.tsx
+++ src/pages/CorrectData.tsx
@@ -1,24 +1,25 @@
-import {
-  IonContent,
-  IonHeader,
-  IonPage,
-  IonButton,
-  IonCard,
-  IonCardHeader,
-  IonCardTitle,
-  IonAvatar,
-  IonCheckbox,
-} from "@ionic/react";
+import { IonContent, IonPage, IonButton, useIonRouter } from "@ionic/react";
 
 const CorrectData: React.FC = () => {
+  const router = useIonRouter();
   return (
     <IonPage>
       <IonContent fullscreen>
         <div>
-          <strong>¡Felicitaciones!</strong>
+          <h3 className="font-bold text-4xl text-center">¡Felicitaciones!</h3>
         </div>
-        <p>Cuéntanos dónde quieres tu dinero</p>
-        <IonButton>Datos bancarios</IonButton>
+        <div className="content">
+          <p className="help-text border-y border-solid border-primary-blue py-6 mb-28">
+            Cuéntanos dónde quieres tu dinero
+          </p>
+          <button
+            className="button-primary mb-16"
+            onClick={() => router.push("")}
+          >
+            Datos bancarios
+          </button>
+          <div className="border-bottom" />
+        </div>
       </IonContent>
     </IonPage>
   );
