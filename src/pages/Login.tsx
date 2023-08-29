7079c3920d7ed68350a898e70d0d9640e06884a3
yesmagaly <yesmagaly@gmail.com>
2023-08-29 16:26:52 -0500

    Update auth pages.

---
+++ src/pages/Login.tsx
@@ -0,0 +1,97 @@
+import { useState } from "react";
+import { IonContent, IonPage, useIonRouter } from "@ionic/react";
+import { RouteComponentProps } from "react-router";
+import { useForm } from "react-hook-form";
+
+import logo from "../assets/icons/logo.svg";
+import Modal from "../components/Modal/Modal";
+import Loader from "../components/Loader/Loader";
+import { useAuth } from "./auth/authContext";
+
+interface LoginProps
+  extends RouteComponentProps<{
+    phone: string;
+  }> { }
+
+type FormValues = {
+  email: number;
+  password: string;
+};
+
+const Login: React.FC<LoginProps> = () => {
+  const { logIn } = useAuth()!;
+  const [isOpen, setIsOpen] = useState(false);
+  const router = useIonRouter();
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { isSubmitting, errors },
+  } = useForm();
+
+  const onSubmit = async function (data: FormValues) {
+    const email = data.email;
+    const password = data.password;
+
+    try {
+      await logIn(email, password);
+      router.push("/welcome");
+    } catch (error) {
+      setIsOpen(true);
+      setError("password", { message: error?.message, type: "server" });
+    }
+  };
+
+  return (
+    <IonPage>
+      <IonContent fullscreen>
+        <div className="flex items-center flex-col justify-center text-center h-full">
+          <img className="h-40 mb-20" src={logo} />
+
+          <form className="form mb-14" onSubmit={handleSubmit(onSubmit)}>
+            <input
+              type="email"
+              placeholder="Email"
+              required
+              {...register("email")}
+            />
+
+            <input
+              type="password"
+              placeholder="Password"
+              required
+              maxLength={8}
+              {...register("password")}
+            />
+
+            <button className="block button-primary">Iniciar sesión</button>
+          </form>
+
+          <p className="leading-5 mb-4">
+            ¿Eres nuevo?{" "}
+            <a className="font-semibold" href="#">
+              Registrate aquí
+            </a>
+          </p>
+
+          <p>
+            <a href="/terms-and-conditions" className="text-sm">
+              Términos y condiciones
+            </a>
+          </p>
+        </div>
+
+        <Loader isOpen={isSubmitting} />
+
+        <Modal handleClose={() => setIsOpen(false)} isOpen={isOpen}>
+          <h3 className="font-semibold text-lg mb-5 text-center">
+            Lo sentimos
+          </h3>
+          {<p>{errors?.password?.message}</p>}
+        </Modal>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Login;
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,37 +1,123 @@
+import { useState } from "react";
 import { IonContent, IonPage, useIonRouter } from "@ionic/react";
+import { useForm, Controller } from "react-hook-form";
+import Modal from "../components/Modal/Modal";
+import Loader from "../components/Loader/Loader";
+import { useAuth } from "./auth/authContext";
+
+type FormValues = {
+  password: string | undefined;
+  password_confirmation: string | undefined;
+};
+
+const validate = function (values: FormValues) {
+  const errors = {};
+
+  if (!values.password) {
+    errors.password = {
+      type: "required",
+      message: "The cellphone number is required.",
+    };
+  } else if (
+    !/^\+[0-9]{2}\s\([0-9]{3}\)\s[0-9]{3}-[0-9]{3}$/g.test(values.phone)
+  ) {
+    errors.password = {
+      type: "pattern",
+      message: "Invalid cellphone number.",
+    };
+  }
+
+  return errors;
+};
 
 const Welcome: React.FC = () => {
+  const { authInfo } = useAuth()!;
   const router = useIonRouter();
+  const [isOpen, setIsOpen] = useState(false);
+  const {
+    register,
+    control,
+    handleSubmit,
+    setError,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
+  const onSubmit = async function (data: FormValues) {
+    // Make validaions.
+    // const errors = validate(data);
+
+    // Show phone errors.
+    // if (errors.phone) {
+    //   setError("phone", errors.phone);
+    //   setIsOpen(true);
+    //   return;
+    // }
+
+    const password = data.password;
+    const password_confirmation = data.password_confirmation;
+
+    // Send phone request.
+    const response = await fetch("http://adelantto-server.docksal/api/user/update-password", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: token,
+      },
+      body: JSON.stringify({ password, password_confirmation }),
+    });
+
+    const json = await response.json();
+
+    if (json.status === "success") {
+      // router.push(`/verification-code/${phone}`);
+      // () => router.push("/verification-email")
+    } else {
+      // Show server errors.
+      setError("password", { message: json.message, type: "server" });
+      setIsOpen(true);
+    }
+  };
+
   return (
     <IonPage>
       <IonContent fullscreen>
-        <div>
-          <h1 className="heading__title">¡Bienvenido!</h1>
-          <p className="w-44 mb-14 leading-5">
-            Ahora eres parte <br />
-            <strong> de Adelantto </strong>
-          </p>
-        </div>
-        <div className="content">
-          <p>Actualiza tu contraseña</p>
-          <form className="form mb-7">
-            <input type="text" placeholder="Nueva contraseña" />
-          </form>
-          <form className="form mb-7">
-            <input type="text" placeholder="Repite tu nueva contraseña" />
-          </form>
-          <div>
-            <a className="underline"> Validación de password*</a>
+        <div className="py-6">
+          <div className="flex flex-col items-center">
+            <h1 className="heading__title text-center">¡Bienvenido!</h1>
+            <p className="text-center w-44 mb-14 leading-5">
+              Ahora eres parte <br />
+              <strong> de Adelantto </strong>
+            </p>
           </div>
 
-          <button
-            className="button-primary mb-16"
-            onClick={() => router.push("/verification-email")}
-          >
-            Cambiar contraseña
-          </button>
-          <div className="border-bottom" />
+          <div className="content">
+            <p>Actualiza tu contraseña</p>
+            <form className="form mb-16" onSubmit={handleSubmit(onSubmit)}>
+              <input
+                {...register("password")}
+                type="password"
+                placeholder="Nueva contraseña"
+              />
+              <input
+                {...register("password_confirmation")}
+                type="password"
+                placeholder="Repite tu nueva contraseña"
+              />
+              <p className="mb-4"><a className="underline"> Validación de password*</a></p>
+              <button className="button-primary mb-16">Cambiar contraseña</button>
+            </form>
+            <div className="border-bottom" />
+          </div>
         </div>
+
+        <Loader isOpen={isSubmitting} />
+        <Modal handleClose={() => setIsOpen(false)} isOpen={isOpen}>
+          <h3 className="font-semibold text-lg mb-5 text-center">
+            Lo sentimos
+          </h3>
+          {<p>{errors?.password?.message}</p>}
+        </Modal>
       </IonContent>
     </IonPage>
   );
