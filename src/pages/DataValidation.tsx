9eb26c754f9d8fa325e442dbaee9d2fbd38c3bd6
juvenal <juanjuvenaldo@gmail.com>
2023-08-30 17:51:53 -0500

    Added style and text center.

--- src/pages/DataValidation.tsx
+++ src/pages/DataValidation.tsx
@@ -1,7 +1,6 @@
 import {
   IonContent,
   IonPage,
-  IonButton,
   IonCard,
   IonCardHeader,
   IonCardTitle,
@@ -14,39 +13,48 @@ const DataValidation: React.FC = () => {
   return (
     <IonPage>
       <IonContent fullscreen>
-        <IonCard>
-          <img
-            alt="Silhouette of mountains"
-            src="https://ionicframework.com/docs/img/demos/card-media.png"
-          />
-          <IonCardHeader>
-            <IonCardTitle>¡Ya casi!</IonCardTitle>
-          </IonCardHeader>
-        </IonCard>
-        <div>
-          <p className="text-sm leading-4">
-            Por último validaremos los datos de tu propiedad. Te
+        <div className="content">
+          <IonCard>
+            <img
+              alt="Silhouette of mountains"
+              src="https://ionicframework.com/docs/img/demos/card-media.png"
+            />
+            <IonCardHeader>
+              <IonCardTitle>¡Ya casi!</IonCardTitle>
+            </IonCardHeader>
+          </IonCard>
+        </div>
+        <div className="heading--center">
+          <form className="form">
+            <div className="mb-7">
+              <p className="text-sm leading-4">
+                Por último validaremos los datos de tu propiedad. Te
+                <br />
+                enviaremos un correo electrónico en las próximas <br />
+                <strong>72 horas con el resultado de la validación.</strong>
+              </p>
+            </div>
+          </form>
+          <div className="mb-7">
+            <IonCheckbox labelPlacement="end">
+              Acepto que “S.A. de C.V.” <br /> consulte mi buró de crédito
+            </IonCheckbox>{" "}
             <br />
-            enviaremos un correo electrónico en las próximas <br />
-            <strong>72 horas con el resultado de la validación.</strong>
-          </p>
+            <IonCheckbox labelPlacement="end">SMS</IonCheckbox> <br />
+            <IonCheckbox labelPlacement="end">Push</IonCheckbox>
+          </div>
+
+          <div className="mb-7">
+            <strong>¡Hasta pronto!</strong>
+          </div>
+          <button
+            className="button-primary mb-16"
+            onClick={() => router.push("")}
+          >
+            Finalizar
+          </button>
+          <div className="border-bottom" />
         </div>
-        <IonCheckbox labelPlacement="end">
-          Acepto que “S.A. de C.V.” <br /> consulte mi buró de crédito
-        </IonCheckbox>{" "}
-        <br />
-        <IonCheckbox labelPlacement="end">SMS</IonCheckbox> <br />
-        <IonCheckbox labelPlacement="end">Push</IonCheckbox>
-        <div>
-          <strong>¡Hasta pronto!</strong>
-        </div>
-        <button
-          className="button-primary mb-16"
-          onClick={() => router.push("")}
-        >
-          Finalizar
-        </button>
-        <div className="border-bottom" />
       </IonContent>
     </IonPage>
   );
