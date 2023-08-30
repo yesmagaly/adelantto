c4ece35162aa9678c6fb3c737fd60b1383168a74
juvenal <juanjuvenaldo@gmail.com>
2023-08-30 11:32:30 -0500

    Added background blue and style to title-content.

--- src/pages/PreOffer.tsx
+++ src/pages/PreOffer.tsx
@@ -1,53 +1,64 @@
-import {
-  IonContent,
-  IonHeader,
-  IonPage,
-  IonButton,
-  IonCard,
-  IonCardHeader,
-  IonCardTitle,
-} from "@ionic/react";
+import { IonContent, IonPage, IonButton } from "@ionic/react";
 
 const PreOffer: React.FC = () => {
   return (
     <IonPage>
       <IonContent fullscreen>
-        <div>
-          <strong>Pre-Oferta Adelantto</strong>
+        <div className="heading heading--blue heading--center heading--compact">
+          <h2 className="text-[30px]">
+            Pre<strong>-Oferta Adelantto</strong>
+          </h2>
         </div>
-        <div>
-          <strong>
-            Seleccionaste 6 meses, <br /> a continuación te presentamos <br />{" "}
-            el resumen de nuestra oferta.
-          </strong>
-        </div>
-        <div>
-          <button>
-            $ 30,000.00 MXN <br />
-            El total de los meses de tu renta
-          </button>
-          <button>
-            $ 28,000.00 MXN <br />
-            El monto que recibirás
-          </button>
-          <button>
-            $ 2,000.00 MXN <br />
-            Costo de nuestro servicio y seguro
-          </button>
-        </div>
-        <strong>¿Te interesa ver el reumen para otros meses?</strong>
-        <form action="">
-          <input type="text" placeholder="6" />
-        </form>
+        <div className="content">
+          <form className="form">
+            <div className="mb-7">
+              <p>
+                Seleccionaste 6 meses,
+                <strong>
+                  <br /> a continuación te presentamos <br /> el resumen de
+                  nuestra oferta.
+                </strong>
+              </p>
+            </div>
 
-        <IonButton>
-          Iniciar proceso de Validación <br />
-          de documentos
-        </IonButton>
-        <p>
-          Al aceptar estas aprovando el uso de tus datos <br />
-          para validaciones de identificación. <a href="">Ver detalle</a>
-        </p>
+            <div>
+              <input
+                type="text"
+                placeholder="$ 30,000.00 MXN 
+                El total de los meses de tu renta"
+              />
+              <input
+                type="text"
+                placeholder="$ 28,000.00 MXN
+                El monto que recibirás"
+              />
+              <input
+                type="text"
+                placeholder="$ 2,000.00 MXN
+                Costo de nuestro servicio y seguro"
+              />
+            </div>
+          </form>
+        </div>
+        <div className="content">
+          <p className="help-text mb-4">
+            ¿Te interesa ver el reumen para otros meses?
+          </p>
+
+          <form className="form">
+            <input type="text" placeholder="6" />
+          </form>
+
+          <IonButton>
+            Iniciar proceso de Validación <br />
+            de documentos
+          </IonButton>
+          <p className="heading__paper">
+            Al aceptar estas aprovando el uso de tus datos <br />
+            para validaciones de identificación.{" "}
+            <a className="underline">Ver detalle</a>
+          </p>
+        </div>
       </IonContent>
     </IonPage>
   );
