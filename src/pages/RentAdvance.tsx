4713b3c8fb7386a0c5276c223fb834d9c475635f
juvenal <juanjuvenaldo@gmail.com>
2023-08-30 08:28:34 -0500

    Added style to title and button.

--- src/pages/RentAdvance.tsx
+++ src/pages/RentAdvance.tsx
@@ -5,40 +5,45 @@ const RentAdvance: React.FC = () => {
   return (
     <IonPage>
       <IonContent fullscreen>
-        <div>
-          <h3>
+        <div className="heading heading--blue heading--center">
+          <h3 className="text-[30px]">
             Selecciona el monto <br />{" "}
             <strong>
               que te gustaría que <br /> adelentemos de rentas
             </strong>
           </h3>
         </div>
-        <div>
-          <button>$ 5,000.00</button>{" "}
-        </div>
-        <div>
-          <button>$ 10,000.00</button>{" "}
-        </div>
-        <div>
-          <button>$ 15,000.00</button>{" "}
-        </div>
-        <div>
-          <button>$ 20,000.00</button>{" "}
-        </div>
-        <div>
-          <button>$ 25,000.00</button>{" "}
-        </div>
-        <div>
-          <button>$ 30,000.00</button>{" "}
-        </div>
+        <div className="content">
+          <form>
+            <div className="mb-7">
+              <button>$ 5,000.00</button>{" "}
+            </div>
+            <div className="mb-7">
+              <button>$ 10,000.00</button>{" "}
+            </div>
+            <div className="mb-7">
+              <button>$ 15,000.00</button>{" "}
+            </div>
+            <div className="mb-7">
+              <button>$ 20,000.00</button>{" "}
+            </div>
+            <div className="mb-7">
+              <button>$ 25,000.00</button>{" "}
+            </div>
+            <div className="mb-7">
+              <button>$ 30,000.00</button>{" "}
+            </div>
 
-        <button
-          className="button-primary mb-16"
-          onClick={() => router.push("/verification-code")}
-        >
-          Siguiente
-        </button>
-        <div className="border-bottom" />
+            <button
+              className="button-primary mb-18"
+              onClick={() => router.push("/verification-code")}
+            >
+              Siguiente
+            </button>
+
+            <div className="border-bottom" />
+          </form>
+        </div>
       </IonContent>
     </IonPage>
   );
