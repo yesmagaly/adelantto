a7050585ed30814244d3d09a77fa1f0b1a944ab7
juvenal <juanjuvenaldo@gmail.com>
2023-08-31 16:06:24 -0500

    Added styles to title and justify

--- src/pages/UploadPictures.tsx
+++ src/pages/UploadPictures.tsx
@@ -1,10 +1,11 @@
 import { IonContent, IonPage, IonList, IonItem, IonLabel } from "@ionic/react";
+import FileInputItem from "../components/FileInputItem";
 
 const UploadPictures: React.FC = () => {
   return (
     <IonPage>
       <IonContent fullscreen>
-        <div className="heading heading--blue">
+        <div className="heading heading--blue flex flex-col justify-center">
           <div className="heading__pager text-right">Paso 3 de 3</div>
           <h4 className="text-xl">
             A continuación
@@ -14,46 +15,36 @@ const UploadPictures: React.FC = () => {
             para validar tu propiedad
           </h4>
         </div>
-        <div className="content">
-          <form className="form">
-            <IonList>
-              <IonItem>
-                <IonLabel>
-                  <strong>Frente de la casa</strong>
-                  <p>Toma una foto del frente de tu casa</p>
-                </IonLabel>
-              </IonItem>
-              <IonItem>
-                <IonLabel>
-                  <strong>Medidor de luz</strong>
-                  <p>Toma una foto del medidor de luz</p>
-                </IonLabel>
-              </IonItem>
-              <IonItem>
-                <IonLabel>
-                  <strong>Toma de agua</strong>
-                  <p>Toma una foto de la toma de agua principal</p>
-                </IonLabel>
-              </IonItem>
-              <IonItem>
-                <IonLabel>
-                  <strong>Calle</strong>
-                  <p>Toma una foto de la vista de la calle</p>
-                </IonLabel>
-              </IonItem>
-            </IonList>
-            <div className="text center mb-7">
-              <p className="font-semibold text-[10px] leading-3 mb-4">
-                Los documentos deberán ser escaneados en alta resolución <br />{" "}
-                y en formato PDF, de lo contrario declinaremos el proceso.
-              </p>
-              <button className="bg-primary-green font-semibold py-2 px-11 rounded text-white">
-                Siguiente
-              </button>
-            </div>
-            <div className="border-bottom" />
-          </form>
+        <div className="py-6 mb-40">
+          <FileInputItem>
+            <h5 className="font-bold text-xs leading-3">Frente de la casa</h5>
+            <p className="text-[10px]">Toma una foto del frente de tu casa</p>
+          </FileInputItem>
+          <FileInputItem>
+            <h5 className="font-bold text-xs leading-3">Medidor de luz</h5>
+            <p className="text-[10px]">Toma una foto del medidor de luz</p>
+          </FileInputItem>
+          <FileInputItem>
+            <h5 className="font-bold text-xs leading-3">Toma de agua</h5>
+            <p className="text-[10px]">
+              Toma una foto de la toma de agua principal
+            </p>
+          </FileInputItem>
+          <FileInputItem>
+            <h5 className="font-bold text-xs leading-3">Calle</h5>
+            <p className="text-[10px]">Toma una foto de la vista de la calle</p>
+          </FileInputItem>
         </div>
+        <div className="text-center mb-7">
+          <p className="font-semibold text-[10px] leading-3 mb-4">
+            Los documentos deberán ser escaneados en alta resolución <br /> y en
+            formato PDF, de lo contrario declinaremos el proceso.
+          </p>
+          <button className="bg-primary-green font-semibold py-2 px-11 rounded text-white">
+            Siguiente
+          </button>
+        </div>
+        <div className="border-bottom border-primary-blue" />
       </IonContent>
     </IonPage>
   );
