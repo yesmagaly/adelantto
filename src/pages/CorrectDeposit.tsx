e65ddaa584fd39964817daec8e7320fa0b771178
juvenal <juanjuvenaldo@gmail.com>
2023-08-31 17:59:51 -0500

    Added styles to title and blue light botton.

--- src/pages/CorrectDeposit.tsx
+++ src/pages/CorrectDeposit.tsx
@@ -6,20 +6,28 @@ const CorrectDeposit: React.FC = () => {
     <IonPage>
       <IonContent fullscreen>
         <div>
-          <h1>¡Gracias!</h1>
-        </div>
-        <div>
-          <h3>
-            ¡Gracias por confiar <br /> en nosotros!
+          <h3 className="font-bold text-4xl text-center py-10 mb-7">
+            ¡Gracias!
           </h3>
-          <p>
+        </div>
+        <div className="text-center mb-10">
+          <h3 className="mb-7">
+            <strong>
+              ¡Gracias por confiar <br /> en nosotros!
+            </strong>
+          </h3>
+          <p className="text-sm mb-10">
             El dinero ya está en tu cuenta. Recuerda que al terminar de pagar el
             total de las rentas adelantadas podrás solicitar un nuevo Adelantto.
           </p>
+          <button
+            className=" button bg-primary-blue-light"
+            onClick={() => router.push("")}
+          >
+            Menú
+          </button>
         </div>
-        <button className="button-primary mb-4" onClick={() => router.push("")}>
-          Menú
-        </button>
+        <div className="border-bottom border-primary-blue" />
       </IonContent>
     </IonPage>
   );
