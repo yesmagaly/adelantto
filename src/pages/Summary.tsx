de2c204d34acc2d525fdab91af4dfa4b6100c035
juvenal <juanjuvenaldo@gmail.com>
2023-09-01 11:16:50 -0500

    Added background blue and styles to pages.

--- src/pages/Summary.tsx
+++ src/pages/Summary.tsx
@@ -5,34 +5,39 @@ const Summary: React.FC = () => {
   return (
     <IonPage>
       <IonContent fullscreen>
-        <div>
-          <h5>
-            <strong>Adelantto 00001</strong>
-          </h5>
-          <p>Calle 17 Sur 13-22</p>
+        <div className="py-10">
+          <div className="heading heading--blue flex flex-col justify-center">
+            <h5 className="font-bold text-lx leading-3"> Adelantto 00001</h5>
+            <p className="text-[15px] mb-7">Calle 17 Sur 22</p>
+            <div>
+              <h6 className="mb-4">CUOTA A PAGAR</h6>
+              <p className="font-bold text-lx leading-3 mb-4">$ 10.000</p>
+              <h6 className="mb-4">Saldo</h6>
+              <p className="font-bold text-lx leading-3">$ 30.000</p>
+            </div>
+          </div>
         </div>
-        <div>
-          <h6>CUOTA A PAGAR</h6>
-          <p>$ 10.000</p>
-          <h6>Saldo</h6>
-          <p>$ 30.000</p>
-        </div>
-        <div>
-          <h4>RESUMEN</h4>
-        </div>
-        <div>
-          <button>MES 1</button>
-          <button>MES 2</button>
-          <button>MES 3</button>
-          <button>MES 2</button>
-        </div>
-        <div>
-          <h6>MES 1 - PAGADO</h6>
-          <p>26-04-2023</p>
-          <h6>MES 2 - PAGADO</h6>
-          <p>26-05-2023</p>
-          <h6>MES 3 - PENDIENTE POR PAGAR</h6>
-          <p>26-06-2023</p>
+
+        <div className="px-7">
+          <div>
+            <h4 className="font-bold text-lx mb-4">RESUMEN</h4>
+          </div>
+          <div className="mb-7">
+            <button>MES 1</button>
+            <button>MES 2</button>
+            <button>MES 3</button>
+            <button>MES 2</button>
+          </div>
+          <div>
+            <h6 className="font-bold text-[15px] leading-3">MES 1 - PAGADO</h6>
+            <p className="text-[12px] mb-4">26-04-2023</p>
+            <h6 className="font-bold text-[15px] leading-3">MES 2 - PAGADO</h6>
+            <p className="text-[12px] mb-4">26-05-2023</p>
+            <h6 className="font-bold text-[15px] leading-3">
+              MES 3 - PENDIENTE POR PAGAR
+            </h6>
+            <p className="text-[12px] mb-4">26-06-2023</p>
+          </div>
         </div>
 
         <div>
