1806f103b998e0cfccb63d9160049765c29086ab
juvenal <juanjuvenaldo@gmail.com>
2023-09-01 11:50:30 -0500

    Added styles on profile page.

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -12,7 +12,7 @@ const Profile: React.FC = () => {
   return (
     <IonPage>
       <IonContent fullscreen>
-        <div>
+        <div className="heading__paper text-right h-20 py-7 px-7">
           <button onClick={() => router.push("")}>
             <IonIcon icon={ellipse}></IonIcon>
           </button>
@@ -23,26 +23,31 @@ const Profile: React.FC = () => {
             <IonIcon icon={ellipse}></IonIcon>
           </button>
         </div>
-        <div>
+        <div className="heading--center mb-4">
           <IonAvatar>
             <img
               alt="Silhouette of a person's head"
               src="https://ionicframework.com/docs/img/demos/avatar.svg"
             />
           </IonAvatar>
-          <h5>Alexander Cruz Páez</h5>
+        </div>
+        <div className="text-center">
+          <h5 className="font-bold text-lx leading-5">Alexander Cruz Páez</h5>
           <p>acruz@csoft.co</p>
         </div>
-        <div>
-          <h6>Adelanto</h6>
-          <p>Notificaciones</p>
-          <p>Mis datos</p>
-        </div>
-        <div>
-          <h6>Ayuda</h6>
-          <p>Reportar un problema</p>
-          <p>Preguntas frecuentes</p>
-          <p>Términos y condiciones</p>
+
+        <div className="px-10">
+          <div>
+            <h6 className="font-bold text-xs leading-3 mb-4">Adelanto</h6>
+            <p className="text-[16px] mb-4">Notificaciones</p>
+            <p className="text-[16px] mb-4">Mis datos</p>
+          </div>
+          <div>
+            <h6 className="font-bold text-xs leading-3 mb-4">Ayuda</h6>
+            <p className="text-[16px] mb-4">Reportar un problema</p>
+            <p className="text-[16px] mb-4">Preguntas frecuentes</p>
+            <p className="text-[16px] mb-4">Términos y condiciones</p>
+          </div>
         </div>
       </IonContent>
     </IonPage>
