32d38c4476eb5dd55b9dbf1b2cd45c18eca47b73
juvenal <juanjuvenaldo@gmail.com>
2023-09-02 09:06:33 -0500

    Added styles to title and botton.

--- src/pages/DataValidation.tsx
+++ src/pages/DataValidation.tsx
@@ -14,27 +14,21 @@ const DataValidation: React.FC = () => {
     <IonPage>
       <IonContent fullscreen>
         <div className="content">
-          <IonCard>
-            <img
-              alt="Silhouette of mountains"
-              src="https://ionicframework.com/docs/img/demos/card-media.png"
-            />
-            <IonCardHeader>
-              <IonCardTitle>¡Ya casi!</IonCardTitle>
-            </IonCardHeader>
-          </IonCard>
+          <img
+            alt="Silhouette of mountains"
+            src="https://ionicframework.com/docs/img/demos/card-media.png"
+          />
+          <h3 className="font-bold text-xl">¡Ya casi!</h3>
+          <div className="border-full" />
+          <p className="text-center text-sm leading-4">
+            Por último validaremos los datos de tu propiedad. Te
+            <br />
+            enviaremos un correo electrónico en las próximas <br />
+            <strong>72 horas con el resultado de la validación.</strong>
+          </p>
+          <div className="border-full" />
         </div>
         <div className="heading--center">
-          <form className="form">
-            <div className="mb-7">
-              <p className="text-center text-sm leading-4">
-                Por último validaremos los datos de tu propiedad. Te
-                <br />
-                enviaremos un correo electrónico en las próximas <br />
-                <strong>72 horas con el resultado de la validación.</strong>
-              </p>
-            </div>
-          </form>
           <div className="mb-7">
             <IonCheckbox labelPlacement="end">
               Acepto que “S.A. de C.V.” <br /> consulte mi buró de crédito
@@ -43,11 +37,9 @@ const DataValidation: React.FC = () => {
             <IonCheckbox labelPlacement="end">SMS</IonCheckbox> <br />
             <IonCheckbox labelPlacement="end">Push</IonCheckbox>
           </div>
-
+          <div className="border-full" />
           <div className="mb-7">
-            <h4 className="font-bold text-xl">
-              <strong>¡Hasta pronto!</strong>
-            </h4>
+            <h4 className="font-bold text-xl">¡Hasta pronto!</h4>
           </div>
           <button
             className="button button-primary mb-16"
