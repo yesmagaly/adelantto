814fa9f551576746cd6a0a3d7d082fe9fe494559
juvenal <juanjuvenaldo@gmail.com>
2023-09-04 18:52:39 -0500

    Added heading light green to pages.

--- src/pages/CorrectData.tsx
+++ src/pages/CorrectData.tsx
@@ -5,7 +5,7 @@ const CorrectData: React.FC = () => {
   return (
     <IonPage>
       <IonContent fullscreen>
-        <div className="py-20">
+        <div className="heading-light-green flex p-4 flex-col items-center text-center mb-10 py-20">
           <h3 className="font-bold text-4xl text-center">¡Felicitaciones!</h3>
         </div>
         <div className="content">
