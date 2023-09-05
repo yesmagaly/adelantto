4ec49ab25259e321ba4b32ef10cda478737b49f0
juvenal <juanjuvenaldo@gmail.com>
2023-09-05 07:11:33 -0500

    Added hand-home image animation.

--- src/pages/CorrectDeposit.tsx
+++ src/pages/CorrectDeposit.tsx
@@ -1,4 +1,7 @@
 import { IonContent, IonPage, useIonRouter } from "@ionic/react";
+import Lottie from "react-lottie-player";
+
+import handHomeAnimation from "../assets/animations/hand-home.json";
 
 const CorrectDeposit: React.FC = () => {
   const router = useIonRouter();
@@ -6,28 +9,35 @@ const CorrectDeposit: React.FC = () => {
     <IonPage>
       <IonContent fullscreen>
         <div>
-          <h3 className="font-bold text-4xl text-center py-10 mb-7">
-            ¡Gracias!
-          </h3>
+          <h3 className="font-bold text-4xl text-center py-8">¡Gracias!</h3>
         </div>
-        <div className="text-center mb-10">
-          <h3 className="mb-7">
-            <strong>
+
+        <div className="content">
+          <Lottie
+            animationData={handHomeAnimation}
+            style={{ width: 350, height: 350 }}
+            loop
+            play
+          />
+
+          <div className="text-center mb-10">
+            <h3 className="font-bold text-2xl leading-7 mb-7">
               ¡Gracias por confiar <br /> en nosotros!
-            </strong>
-          </h3>
-          <p className="text-sm mb-10">
-            El dinero ya está en tu cuenta. Recuerda que al terminar de pagar el
-            total de las rentas adelantadas podrás solicitar un nuevo Adelantto.
-          </p>
-          <button
-            className=" button bg-primary-blue-light"
-            onClick={() => router.push("")}
-          >
-            Menú
-          </button>
+            </h3>
+            <p className="text-lg px-5 mb-10">
+              El dinero ya está en tu cuenta. Recuerda que al terminar de pagar
+              el total de las rentas adelantadas podrás solicitar un nuevo
+              Adelantto.
+            </p>
+            <button
+              className=" button bg-primary-blue-light"
+              onClick={() => router.push("")}
+            >
+              Menú
+            </button>
+          </div>
+          <div className="border-bottom border-primary-blue" />
         </div>
-        <div className="border-bottom border-primary-blue" />
       </IonContent>
     </IonPage>
   );
