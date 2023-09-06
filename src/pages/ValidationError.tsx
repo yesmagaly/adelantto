21bcfd6fe013ad7287e32dd3434703256591ec61
juvenal <juanjuvenaldo@gmail.com>
2023-09-05 21:05:28 -0500

    Added close home animation to pages.

--- src/pages/ValidationError.tsx
+++ src/pages/ValidationError.tsx
@@ -1,4 +1,7 @@
 import { IonContent, useIonRouter, IonPage } from "@ionic/react";
+import Lottie from "react-lottie-player";
+
+import closeHomeAnimation from "../assets/animations/close-home.json";
 
 const ValidationError: React.FC = () => {
   const router = useIonRouter();
@@ -6,11 +9,16 @@ const ValidationError: React.FC = () => {
     <IonPage>
       <IonContent fullscreen>
         <div className="content">
-          <h3 className=" font-bold text-4xl text-center">
-            <strong>¡Lo sentimos!</strong>
-          </h3>
+          <h3 className=" font-bold text-4xl">¡Lo sentimos!</h3>
+
+          <Lottie
+            animationData={closeHomeAnimation}
+            style={{ width: 274, height: 274 }}
+            loop
+            play
+          />
         </div>
-        <div className="content">
+        <div className="text-center">
           <p className="text-sm leading-4 border-y border-solid border-primary-blue py-6 mb-28">
             Los datos de tu propiedad no han pasado <br /> nuestro proceso de
             validación
