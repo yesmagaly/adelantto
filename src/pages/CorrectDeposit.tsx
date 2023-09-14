09465b8d78eb7fb321c6a82f32a9c069eb5d6ec9
juvenal <juanjuvenaldo@gmail.com>
2023-09-14 16:41:06 -0500

    Added background to correct deposit page.

--- src/pages/CorrectDeposit.tsx
+++ src/pages/CorrectDeposit.tsx
@@ -8,35 +8,37 @@ const CorrectDeposit: React.FC = () => {
   return (
     <IonPage>
       <IonContent fullscreen>
-        <div>
-          <h3 className="font-bold text-4xl text-center py-8">¡Gracias!</h3>
-        </div>
-
-        <div className="content">
-          <Lottie
-            animationData={handHomeAnimation}
-            style={{ width: 350, height: 350 }}
-            loop
-            play
-          />
-
-          <div className="text-center mb-10">
-            <h3 className="font-bold text-2xl leading-7 mb-7">
-              ¡Gracias por confiar <br /> en nosotros!
-            </h3>
-            <p className="text-lg px-5 mb-10">
-              El dinero ya está en tu cuenta. Recuerda que al terminar de pagar
-              el total de las rentas adelantadas podrás solicitar un nuevo
-              Adelantto.
-            </p>
-            <button
-              className=" button bg-primary-blue-light"
-              onClick={() => router.push("")}
-            >
-              Menú
-            </button>
+        <div className="heading--sky-blue pt-10">
+          <div>
+            <h3 className="font-bold text-4xl text-center py-8">¡Gracias!</h3>
           </div>
-          <div className="border-bottom border-primary-blue" />
+
+          <div className="content">
+            <Lottie
+              animationData={handHomeAnimation}
+              style={{ width: 350, height: 350 }}
+              loop
+              play
+            />
+
+            <div className="text-center mb-10">
+              <h3 className="font-bold text-2xl leading-7 mb-7">
+                ¡Gracias por confiar <br /> en nosotros!
+              </h3>
+              <p className="text-lg px-5 mb-10">
+                El dinero ya está en tu cuenta. Recuerda que al terminar de
+                pagar el total de las rentas adelantadas podrás solicitar un
+                nuevo Adelantto.
+              </p>
+              <button
+                className=" button bg-primary-blue-light"
+                onClick={() => router.push("")}
+              >
+                Menú
+              </button>
+            </div>
+            <div className="border-bottom border-primary-blue" />
+          </div>
         </div>
       </IonContent>
     </IonPage>
