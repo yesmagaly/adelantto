74bfdd6702431357b3875ec77b3c79ea9056a7db
juvenal <juanjuvenaldo@gmail.com>
2023-09-14 12:09:05 -0500

    Added styles to page.

---
+++ src/pages/SuccesfulTransaction.tsx
@@ -0,0 +1,35 @@
+import { IonContent, IonPage, IonButton, useIonRouter } from "@ionic/react";
+
+import check from "../assets/icons/check.png";
+
+const SuccesfulTransaction: React.FC = () => {
+  const router = useIonRouter();
+  return (
+    <IonPage>
+      <IonContent fullscreen>
+        <div className="blue-bg flex items-center flex-col justify-center text-center">
+          <div className="pt-72">
+            <img className="h-14 mb-10 " src={check} />
+          </div>
+
+          <h3 className="font-bold text-green-300 text-3xl text-center h-60">
+            Tu transacción <br />
+            <span className="text-white">ha sido exitosa</span>
+          </h3>
+
+          <div className="py-10">
+            <button
+              className=" button button-primary mb-16"
+              onClick={() => router.push("")}
+            >
+              Finalizar
+            </button>
+            <div className="border-bottom" />
+          </div>
+        </div>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default SuccesfulTransaction;
