2784840f3f93aa2a08d5cae7f4f5240bac504058
juvenal <juanjuvenaldo@gmail.com>
2023-09-15 17:57:33 -0500

    Updated styles in the Summary page.

--- src/pages/Summary.tsx
+++ src/pages/Summary.tsx
@@ -7,7 +7,7 @@ const Summary: React.FC = () => {
   return (
     <IonPage>
       <IonContent fullscreen>
-        <div className="heading__paper flex justify-end gap-3 items-center h-20 py-10 px-7">
+        <div className="heading__paper flex justify-end gap-3 items-center h-20 py-10 px-8">
           <div>
             <button onClick={() => router.push("")}>
               <IonIcon className="w-2 text-gray-300" icon={ellipse}></IonIcon>
@@ -22,44 +22,48 @@ const Summary: React.FC = () => {
             </button>
           </div>
 
-          <Icon name="bell" className="mr-6 text-2xl bg-blue-800" />
+          <Icon name="bell" className="text-2xl bg-blue-800" />
         </div>
-        <div className="py-8">
-          <div className="h-80 bg-cover text-white px-9 py-6 heading--blue flex flex-col justify-center">
-            <h5 className="font-bold text-2xl leading-6">Adelantto 00001</h5>
-            <p className="text-[15px] mb-2">Calle 17 Sur 22</p>
-            <div className="border-full" />
-            <div>
-              <h6 className="mb-10 text-green-400">CUOTA A PAGAR</h6>
-              <p className="font-bold text-2xl leading-3 mb-10">$ 10.000</p>
-              <h6 className="mb-8 text-green-400">Saldo</h6>
-              <p className="font-bold text-2xl leading-3">$ 30.000</p>
-            </div>
+
+        <div className="h-96 bg-cover text-white px-9 py-14 heading--blue">
+          <h5 className="font-bold text-2xl leading-6">Adelantto 00001</h5>
+          <p className="mb-6">Calle 17 Sur 13-22</p>
+          <div className="border-full" />
+          <div>
+            <h6 className="mb-10 mt-5 text-primary-green">CUOTA A PAGAR</h6>
+            <p className="font-bold text-2xl leading-3 mb-10">$ 10.000</p>
+            <h6 className="mb-8 text-primary-green">Saldo</h6>
+            <p className="font-bold text-2xl leading-3">$ 30.000</p>
           </div>
         </div>
 
-        <div className="px-7">
-          <div>
-            <h4 className="font-bold text-lx mb-4 px-4">RESUMEN</h4>
+        <div className="px-7 py-8">
+          <h4 className="font-bold text-lx mb-4">RESUMEN</h4>
+
+          <div className="flex justify-between gap-3 mb-7">
+            <button className="text-lg bg-gray-200 px-4 py-0.5 rounded-md">
+              MES 1
+            </button>
+            <button className="text-lg bg-gray-200 px-4 py-0.5 rounded-md">
+              MES 2
+            </button>
+            <button className="text-lg bg-primary-green px-4 py-0.5 rounded-md">
+              MES 3
+            </button>
+            <button className="text-lg bg-gray-200 px-4 py-0.5 rounded-md">
+              MES 2
+            </button>
           </div>
-          <div className="mb-7 px-4">
-            <button>MES 1</button>
-            <button>MES 2</button>
-            <button>MES 3</button>
-            <button>MES 2</button>
-          </div>
-          <div className="px-4 mb-10">
+          <div className="mb-10">
             <form>
               <div className="flex justify-between items-center">
-                <div className="items-center">
-                  <h6 className="font-bold text-[15px] leading-6">
-                    MES 1 - PAGADO
-                  </h6>
-                  <p className="text-[12px] mb-4">26-04-2023</p>
+                <div className="mb-4">
+                  <h6 className="font-bold leading-6">MES 1 - PAGADO</h6>
+                  <p className="text-xs">26-04-2023</p>
                 </div>
 
                 <button
-                  className="font-regular px-2 py-1 rounded text-white leading-5 button-secondary"
+                  className="font-regular px-2 py-1 rounded text-white button-secondary"
                   onClick={() => router.push("")}
                 >
                   Ver
@@ -67,15 +71,13 @@ const Summary: React.FC = () => {
               </div>
               <div className="border-full" />
               <div className="flex justify-between items-center">
-                <div className="items-center">
-                  <h6 className="font-bold text-[15px] leading-6">
-                    MES 2 - PAGADO
-                  </h6>
-                  <p className="text-[12px] mb-4">26-04-2023</p>
+                <div className="mb-4">
+                  <h6 className="font-bold leading-6">MES 2 - PAGADO</h6>
+                  <p className="text-xs">26-04-2023</p>
                 </div>
 
                 <button
-                  className="font-regular px-2 py-1 rounded text-white leading-5 button-secondary"
+                  className="font-regular px-2 py-1 rounded text-white button-secondary"
                   onClick={() => router.push("")}
                 >
                   Ver
@@ -83,15 +85,15 @@ const Summary: React.FC = () => {
               </div>
               <div className="border-full" />
               <div className="flex justify-between items-center">
-                <div className="items-center">
-                  <h6 className="font-bold text-[15px] leading-6">
-                    MES 3 - PAGADO
+                <div className="mb-4">
+                  <h6 className="font-bold text-blue-900 leading-6">
+                    MES 3 - PENDIENTE POR PAGAR
                   </h6>
-                  <p className="text-[12px] mb-4">26-04-2023</p>
+                  <p className="text-xs">26-04-2023</p>
                 </div>
 
                 <button
-                  className="font-regular px-2 py-1 rounded text-white leading-5 button-secondary"
+                  className="font-regular px-2 py-1 rounded text-white button-secondary"
                   onClick={() => router.push("")}
                 >
                   Ver
