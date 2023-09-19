faecd5e1f041a3d2ce0cdc8d94aabb5bb50c5189
yesmagaly <yesmagaly@gmail.com>
2023-09-19 17:50:02 -0500

    Refactor FileInputItem and ContractData page.

--- src/components/FileInputItem.tsx
+++ src/components/FileInputItem.tsx
@@ -12,7 +12,9 @@ const UploadDocuments: React.FC<ComponentProp> = ({ children, icon }) => {
         <div className="flex justify-center items-center basis-32 border-b border-r-2 border-[#D8D8D8] border-solid">
           <div className="flex items-center gap-5">
             <input type="checkbox" id="upload" />
-            <Icon name={icon} className="bg-gray-300" />
+            <button>
+              <Icon name={icon} className="bg-gray-300" />
+            </button>
           </div>
         </div>
         <div className="my-4 basis-full leading-3">{children}</div>
--- src/pages/ContractData.tsx
+++ src/pages/ContractData.tsx
@@ -1,82 +1,83 @@
-import { IonContent, IonPage, IonCheckbox, useIonRouter } from "@ionic/react";
+import { IonContent, IonPage, useIonRouter } from "@ionic/react";
 
 const ContractData: React.FC = () => {
   const router = useIonRouter();
   return (
     <IonPage>
       <IonContent fullscreen>
-        <div className="heading heading--blue heading--center mb-7">
-          <h1 className="text-[40px]">
+        <div className="heading heading--blue heading--center mb-10">
+          <h1 className="text-4xl">
             Datos de contrato <strong>de arrendamiento</strong>
           </h1>
         </div>
 
         <div>
           <form className="form">
-            <div className="mb-6">
-              <input
-                type="text"
-                placeholder="valor de tu renta mensual"
-                className="!mb-0"
-              />
-              <div className="border-full" />
+            <div>
+              <input type="text" placeholder="valor de tu renta mensual" />
+            </div>
+            <div className="border-full !mt-0" />
 
+            <div>
               <input
                 type="text"
                 placeholder="Fecha de inicio del contrato de arrendamiento"
-                className="!mb-0"
               />
-              <div className="border-full" />
+            </div>
+            <div className="border-full !mt-0" />
 
+            <div>
               <input
                 type="text"
                 placeholder="Fecha de fin del contrato de arrendamiento"
-                className="!mb-0"
               />
-              <div className="border-full" />
-
-              <input
-                type="text"
-                placeholder="Código postal de tu inmueble"
-                className="!mb-0"
-              />
-
-              <div className="border-full" />
             </div>
+            <div className="border-full !mt-0" />
 
             <div>
-              <p className="mb-6 font-bold text-[16px]">
+              <input type="text" placeholder="Código postal de tu inmueble" />
+            </div>
+            <div className="border-full !mt-0" />
+
+            <div className="mt-6">
+              <h4 className="mb-6 font-bold">
                 ¿Cómo recibes el pago de tu renta?
-              </p>
+              </h4>
               <div className="flex items-center justify-between mb-10">
-                <div className="flex items-center">
-                  <input type="radio" />
-                  <label
-                    htmlFor="scales"
-                    className="text-[12px] font-bold ml-2"
-                  >
-                    {" "}
-                    Efectivo{" "}
+                <div className="flex items-center gap-2">
+                  <input
+                    type="radio"
+                    id="payment_cash"
+                    name="rent_payment"
+                    value="cash"
+                  />
+                  <label htmlFor="payment_cash" className="text-xs font-bold">
+                    Efectivo
                   </label>
                 </div>
-                <div className="flex items-center">
-                  <input type="radio" />
+                <div className="flex items-center gap-2">
+                  <input
+                    type="radio"
+                    id="payment_transfer"
+                    name="rent_payment"
+                    value="transfer"
+                  />
                   <label
-                    htmlFor="scales"
-                    className="text-[12px] font-bold ml-2"
+                    htmlFor="payment_transfer"
+                    className="text-xs font-bold"
                   >
-                    {" "}
-                    Transferencia{" "}
+                    Transferencia
                   </label>
                 </div>
               </div>
             </div>
             <button
-              className="button button-primary mb-16"
+              className="button button-primary mb-10"
               onClick={() => router.push("/verification-email")}
             >
               Siguiente
             </button>
+            <div className="border-bottom border-primary-blue" />
           </form>
         </div>
       </IonContent>
