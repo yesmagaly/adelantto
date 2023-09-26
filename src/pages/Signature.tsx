c5fe256bf56074754d9f994e9ecc18a6180330b8
juvenal <juanjuvenaldo@gmail.com>
2023-09-26 17:19:02 -0500

    Updated styles the background to Signature page.

--- src/pages/Signature.tsx
+++ src/pages/Signature.tsx
@@ -9,25 +9,27 @@ const Signature: React.FC = () => {
     <IonPage>
       <IonContent fullscreen>
         <div className="content">
-          <form className="form">
-            <div className="pt-10">
-              <h3 className="font-bold text-4xl">¡Firma y recibe!</h3>
-              <Lottie
-                animationData={singAnimation}
-                style={{ width: 300, height: 300 }}
-              />
-            </div>
-            <div className=" mb-5">
-              <input type="text" placeholder="Firma aquí" />
-            </div>
-            <button
-              className="button button-primary mb-16"
-              onClick={() => router.push("")}
-            >
-              Firmar
-            </button>
-            <div className="border-bottom border-primary-blue" />
-          </form>
+          <div className="bg-white py-5 shadow-2xl w-96 border border-gray-200">
+            <form className="form">
+              <div className="pt-10">
+                <h3 className="font-bold text-4xl">¡Firma y recibe!</h3>
+                <Lottie
+                  animationData={singAnimation}
+                  style={{ width: 300, height: 300 }}
+                />
+              </div>
+              <div className=" mb-5">
+                <input type="text" placeholder="Firma aquí" />
+              </div>
+              <button
+                className="button button-primary mb-16"
+                onClick={() => router.push("")}
+              >
+                Firmar
+              </button>
+              <div className="border-bottom border-primary-blue" />
+            </form>
+          </div>
         </div>
       </IonContent>
     </IonPage>
