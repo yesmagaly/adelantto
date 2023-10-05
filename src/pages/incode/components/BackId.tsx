45329d538c939d812e206258286869eb62dd434d
juvenal <juanjuvenaldo@gmail.com>
2023-10-05 17:52:49 -0500

    Added image to BackId page.

--- src/pages/incode/components/BackId.tsx
+++ src/pages/incode/components/BackId.tsx
@@ -1,42 +1,49 @@
 import { useState } from "react";
-import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import {
+  Camera,
+  CameraResultType,
+  CameraSource,
+  Photo,
+} from "@capacitor/camera";
 
 import { addFrontId, addBackId, addFaceSelfie } from "../client";
 import Icon from "../../../components/Icon/Icon";
 import Modal from "../../../components/modal";
-import "../styles.css"
+import "../styles.css";
 
 const errorsMap = {
-  'UNKNOWN_DOCUMENT_TYPE': "document classification failed",
-  'WRONG_DOCUMENT_SIDE': "can happen when uploading back side of id when front id is required or the other way around",
-  'WRONG_ONE_SIDED_DOCUMENT': "uploading wrong document with only one side",
-  'DOCUMENT_NOT_READABLE': "document couldn't be read, probably due to image quality",
-  'UNABLE_TO_ALIGN_DOCUMENT': "alignment failed",
-  'ID_TYPE_UNACCEPTABLE': "invalid type of id",
-  'UNEXPECTED_ERROR_OCCURRED': "unexpected error",
-}
+  UNKNOWN_DOCUMENT_TYPE: "document classification failed",
+  WRONG_DOCUMENT_SIDE:
+    "can happen when uploading back side of id when front id is required or the other way around",
+  WRONG_ONE_SIDED_DOCUMENT: "uploading wrong document with only one side",
+  DOCUMENT_NOT_READABLE:
+    "document couldn't be read, probably due to image quality",
+  UNABLE_TO_ALIGN_DOCUMENT: "alignment failed",
+  ID_TYPE_UNACCEPTABLE: "invalid type of id",
+  UNEXPECTED_ERROR_OCCURRED: "unexpected error",
+};
 
 export interface ComponentProp {
-  children: string | JSX.Element | JSX.Element[],
-  session: any,
-  onSuccess: any,
+  children: string | JSX.Element | JSX.Element[];
+  session: any;
+  onSuccess: any;
 }
 export const BackId: React.FC<ComponentProp> = ({ session, ...props }) => {
   const [photo, setPhoto] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-  const [status, setStatus] = useState('');
+  const [status, setStatus] = useState("");
   const [step, setStep] = useState(0);
 
   const startStep = () => {
-    setStep(0)
-  }
+    setStep(0);
+  };
 
   const takePhoto = async () => {
     const newPhoto = await Camera.getPhoto({
       resultType: CameraResultType.Base64,
       source: CameraSource.Camera,
-      quality: 100
+      quality: 100,
     });
 
     if (newPhoto?.base64String) {
@@ -50,35 +57,38 @@ export const BackId: React.FC<ComponentProp> = ({ session, ...props }) => {
     setLoading(true);
 
     try {
-      const data = await addBackId({ session, body: { base64Image: photo.base64String } });
-      setStatus('is-success');
+      const data = await addBackId({
+        session,
+        body: { base64Image: photo.base64String },
+      });
+      setStatus("is-success");
       setStep(-1);
-      props.onSuccess(data)
+      props.onSuccess(data);
     } catch (error) {
-      setStatus('is-fail');
+      setStatus("is-fail");
       setError(error?.message);
     }
 
     setLoading(false);
-  }
+  };
 
   const tryAgain = () => {
-    clear()
-    takePhoto()
-  }
+    clear();
+    takePhoto();
+  };
 
   const clear = () => {
     setPhoto(null);
     setError(null);
-    setStatus('');
+    setStatus("");
     setStep(-1);
-  }
+  };
 
   return (
     <div>
       <Modal isOpen={step === 0}>
         <h3>Ahora toca el back side</h3>
-        <video></video>
+        <img src="/../src/assets/video/tutorial.gif"></img>
         <button onClick={takePhoto}>Continuar</button>
       </Modal>
 
@@ -86,10 +96,14 @@ export const BackId: React.FC<ComponentProp> = ({ session, ...props }) => {
         <>
           <Modal isOpen={Boolean(photo) && step === 1}>
             <h2 className="heading-3">Revisa tu foto</h2>
-            <p>Asegúrate de que las letras sean claras y tenga buena iluminación.</p>
+            <p>
+              Asegúrate de que las letras sean claras y tenga buena iluminación.
+            </p>
 
             <div>
-              <img src={`data:image/${photo.format};base64,${photo.base64String}`}></img>
+              <img
+                src={`data:image/${photo.format};base64,${photo.base64String}`}
+              ></img>
             </div>
 
             <button onClick={takePhoto}>volver a Capturar</button>
@@ -98,14 +112,18 @@ export const BackId: React.FC<ComponentProp> = ({ session, ...props }) => {
 
           <Modal isOpen={Boolean(photo) && step === 2}>
             <div className={status}>
-              <img src={`data:image/${photo.format};base64,${photo.base64String}`}></img>
+              <img
+                src={`data:image/${photo.format};base64,${photo.base64String}`}
+              ></img>
             </div>
 
             {loading && <div>Cargando ...</div>}
-            {error && <div>
-              <div>La verificación frontal de identificación fall'o</div>
-              <button onClick={tryAgain}>Capturar otra vez</button>
-            </div>}
+            {error && (
+              <div>
+                <div>La verificación frontal de identificación fall'o</div>
+                <button onClick={tryAgain}>Capturar otra vez</button>
+              </div>
+            )}
           </Modal>
         </>
       )}
