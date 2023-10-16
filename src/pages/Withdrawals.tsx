6afba454b2b43ebf3c373252a1fe04ef66524fca
juvenal <juanjuvenaldo@gmail.com>
2023-10-16 18:52:25 -0500

    added formatting for the page modal.

--- src/pages/Withdrawals.tsx
+++ src/pages/Withdrawals.tsx
@@ -1,10 +1,15 @@
 import { IonContent, IonPage, useIonRouter } from "@ionic/react";
 import Lottie from "react-lottie-player";
 
+import * as Modal from "../components/modal";
 import walletAnimation from "../assets/animations/wallet.json";
 import lensAnimation from "../assets/animations/lens.json";
+import { useState } from "react";
+import { fastFoodOutline } from "ionicons/icons";
 
 const Withdrawals: React.FC = () => {
+  const [open, setOpen] = useState(false);
+  const [showModal, setShowModal] = useState(false);
   const router = useIonRouter();
   return (
     <IonPage>
@@ -30,7 +35,9 @@ const Withdrawals: React.FC = () => {
               />
             </form>
             <div className="mb-16">
-              <a className="underline leading-10">Términos interbancarios</a>
+              <a onClick={() => setOpen(true)} className="underline leading-10">
+                Términos interbancarios
+              </a>
               <p>
                 A continuación encontrarás el contrato <br /> relacionado a tu
                 solicitud.
@@ -50,54 +57,67 @@ const Withdrawals: React.FC = () => {
           <div className="border-bottom border-primary-blue" />
         </div>
 
-        <div className="py-10">
-          <h2>
-            <strong>
-              Términos <br /> interbancarios
-            </strong>
-          </h2>
-          <h5>
-            <strong>
-              1. Lorem ipsum dolor sit amet,
-              <br /> consectetuer adipiscing elit, sed
-            </strong>
-          </h5>
-          <p>
-            diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam
-            erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci
-            tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
-            consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate
-            velit esse molestie consequat, vel illum dolore eu feugiat nulla
-            facilisis at vero eros et accumsan et iusto odio dignissim qui
-            blandit praesent luptatum zzril delenit augue duis dolore te feugait
-            nulla facilisi. Lorem ipsum dolor sit amet, cons ectetuer adipiscing
-            elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore
-            magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
-            nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip
-            ex ea commodo consequat.
-          </p>
-        </div>
+        <Modal.Root isOpen={open}>
+          <Modal.Header>
+            <h2>
+              <strong>
+                Términos <br /> interbancarios
+              </strong>
+            </h2>
+          </Modal.Header>
+          <Modal.Body>
+            <h5>
+              <strong>
+                1. Lorem ipsum dolor sit amet,
+                <br /> consectetuer adipiscing elit, sed
+              </strong>
+            </h5>
+            <p>
+              diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+              aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud
+              exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea
+              commodo consequat. Duis autem vel eum iriure dolor in hendrerit in
+              vulputate velit esse molestie consequat, vel illum dolore eu
+              feugiat nulla facilisis at vero eros et accumsan et iusto odio
+              dignissim qui blandit praesent luptatum zzril delenit augue duis
+              dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, cons
+              ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt
+              ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad
+              minim veniam, quis nostrud exerci tation ullamcorper suscipit
+              lobortis nisl ut aliquip ex ea commodo consequat.
+            </p>
+          </Modal.Body>
+          <Modal.Footer>
+            <button onClick={() => setOpen(false)}>Cerrar</button>
+          </Modal.Footer>
+        </Modal.Root>
 
-        <div className="content">
-          <Lottie
-            animationData={lensAnimation}
-            style={{ width: 219, height: 219 }}
-            loop
-            play
-          />
-          <p className="text-[16px] mb-6 leading-5">
-            En este punto estamos validando <br />
-            el convenio con tu entidad bancaria,
-            <br />
-            para cualquier disposición de <br /> efectivo necesaria.
-          </p>
-          <button
-            className="button is-secondary mb-8"
-            onClick={() => router.push("")}
-          >
-            Siguiente
-          </button>
-        </div>
+        <Modal.Root isOpen={showModal}>
+          <Modal.Header></Modal.Header>
+          <Modal.body>
+            <Lottie
+              animationData={lensAnimation}
+              style={{ width: 219, height: 219 }}
+              loop
+              play
+            />
+            <p className="text-[16px] mb-6 leading-5">
+              En este punto estamos validando <br />
+              el convenio con tu entidad bancaria,
+              <br />
+              para cualquier disposición de <br /> efectivo necesaria.
+            </p>
+          </Modal.body>
+          <Modal.Footer>
+            <button
+              className="button is-secondary mb-8"
+              onClick={() => setShowModal(false)}
+            >
+              Siguiente
+            </button>
+          </Modal.Footer>
+        </Modal.Root>
+        <div className="content"></div>
       </IonContent>
     </IonPage>
   );
