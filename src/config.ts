c8a02e50b73d37493a8159144a2d5cf9f30bc653
yesmagaly <yesmagaly@gmail.com>
2023-10-17 14:22:58 -0500

    Updated configs.

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,8 @@
-export const API_SERVER_URL = "https://a3cd-201-240-244-110.ngrok-free.app";
+export const API_SERVER_URL =
+  "http://ldb-back-adelantto-514447670.us-east-1.elb.amazonaws.com";
 
-export const VITE_INCODE_API_URL="https://demo-api.incodesmile.com"
-export const VITE_INCODE_CLIENT_ID="26mf229"
-export const VITE_INCODE_SDK_URL="https://sdk.incode.com/sdk/onBoarding-1.57.0.js"
-export const VITE_INCODE_FLOW_ID="64f1111ac0388f9789122742"
+export const VITE_INCODE_API_URL = "https://demo-api.incodesmile.com";
+export const VITE_INCODE_CLIENT_ID = "26mf229";
+export const VITE_INCODE_SDK_URL =
+  "https://sdk.incode.com/sdk/onBoarding-1.57.0.js";
+export const VITE_INCODE_FLOW_ID = "64f1111ac0388f9789122742";
