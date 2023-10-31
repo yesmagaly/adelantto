afe17b71ce678d5bddb128740da22a52027529da
yesmagaly <yesmagaly@gmail.com>
2023-10-30 20:28:32 -0500

    Updated API server.

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,5 @@
-export const API_SERVER_URL = "http://ldb-back-adelantto-514447670.us-east-1.elb.amazonaws.com";
-
+export const API_SERVER_URL = "https://dev-api.adelantto.com";
 export const VITE_INCODE_API_URL = "https://demo-api.incodesmile.com";
 export const VITE_INCODE_CLIENT_ID = "26mf229";
-export const VITE_INCODE_SDK_URL =
-  "https://sdk.incode.com/sdk/onBoarding-1.57.0.js";
+export const VITE_INCODE_SDK_URL = "https://sdk.incode.com/sdk/onBoarding-1.57.0.js";
 export const VITE_INCODE_FLOW_ID = "64f1111ac0388f9789122742";
