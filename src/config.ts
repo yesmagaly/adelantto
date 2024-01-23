c3f8e0df5eb8fe9b356b137a5914198277038ff7
yesmagaly <yesmagaly@gmail.com>
2024-01-23 08:13:17 -0500

    Updated server url

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const API_SERVER_URL = "http://adelantto-server.docksal";
+export const API_SERVER_URL = "https://dev-api.adelantto.com";
 export const VITE_INCODE_API_URL = "https://demo-api.incodesmile.com";
 export const VITE_INCODE_CLIENT_ID = "26mf229";
 export const VITE_INCODE_SDK_URL = "https://sdk.incode.com/sdk/onBoarding-1.57.0.js";
