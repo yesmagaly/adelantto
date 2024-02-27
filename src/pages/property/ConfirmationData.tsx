11aeb7e88389732d2bbfb0840feaa9c2369a9d77
yesmagaly <yesmagaly@gmail.com>
2024-02-27 18:36:30 -0500

    Updated confirmation data page

--- src/pages/property/ConfirmationData.tsx
+++ src/pages/property/ConfirmationData.tsx
@@ -28,32 +28,38 @@ const ConfirmationData: React.FC = ({ match }) => {
         <div className="heading heading--blue flex flex-col justify-center mb-10">
           <div className="heading__pager text-right"> Paso 3 de 3</div>
           <h4 className="text-xl">
-            <strong>
-              Confirmación de datos <br />y solicitud de consulta <br />
-            </strong>
-            Reporte de Crédito
+            Autorización para solicitar reportes de crédito, informes buró y reportes de crédito especiales
           </h4>
         </div>
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-7 px-9">
             <p className="mb-4">
-              Autorizo expresamente a Adelantto, para que lleve a cabo
-              investigaciones sobre el comportamiento crediticio de mi persona,
-              con las Sociedades de Información Crediticia: Bla, bla, bla, S.A.
-              y/o Trans Banco de México, S.A., durante los siguientes tres años
-              o durante el tiempo que mantenga relación jurídica con Adelantto.
+              Por este conducto autorizo expresamente a <strong>SOLUCIONES INTEGRALES TAFS, SAPI
+              DE C.V.</strong> para que por conducto de sus funcionarios facultados lleve a cabo investigaciones
+              sobre mi comportamiento crediticio a través de Trans Unión de México, S.A. Sociedad de
+              Información Crediticia.
             </p>
             <p className="mb-4">
-              Así mismo, declaro que conozco la naturaleza y alcance de la
-              información que se solicitará, del uso que Adelantto hará de tal
-              información y de que ésta podrá realizar consultas periódicas del
-              historial crediticio de mi persona, durante los siguientes tres
-              años o durante el tiempo que mantengamos relación jurídica.
+              Asimismo, declaro que conozco la naturaleza y alcance de las sociedades de información
+              crediticia y de la información contenida en los informes buró, reportes de crédito y reporte
+              de crédito especial, declaro que conozco la naturaleza y alcance de la información que se
+              solicitará, del uso que <strong>SOLUCIONES INTEGRALES TAFS, SAPI DE C.V.</strong> hará de tal
+              información y de que ésta podrá realizar consultas periódicas sobre mi historial,
+              consintiendo que esta autorización se encuentre vigente por un período de 3 años contados
+              a partir de su expedición y en todo caso durante el tiempo que se mantenga la relación
+              jurídica.
+            </p>
+            <p className="mb-4">
+              En caso de que la solicitante sea una Persona Moral, declaro bajo protesta de decir verdad
+              ser Representante Legal de la empresa mencionada en esta autorización; manifestando que
+              a la fecha de firma de la presente autorización los poderes no me han sido revocados,
+              limitados, ni modificados en forma alguna.
             </p>
             <p>
-              De igual manera corroboró que toda la información aquí expresada
-              es verídica y esta a dispocisión y consulta para fines prácticos
-              de Adelantto y sus servicios.
+              El presente documento, así como los informes buró, reportes de crédito y reporte de crédito
+              especial quedarán en custodia de <strong>SOLUCIONES INTEGRALES TAFS, SAPI DE C.V.</strong> y
+              los funcionarios autorizados, quienes utilizarán la información únicamente para los fines
+              para los cuales fue recabada y se considerará como Confidencial.
             </p>
           </div>
           <div className="flex items-center gap-2 mb-10 px-9">
@@ -64,7 +70,7 @@ const ConfirmationData: React.FC = ({ match }) => {
               required
               {...register('accept_privacy_policy')}
             />
-            <label htmlFor="terms_conditions" className="text-xs font-bold">
+            <label htmlFor="terms_conditions" className="text-sm font-semibold">
               Acepto términos y condiciones del servicio
             </label>
           </div>
