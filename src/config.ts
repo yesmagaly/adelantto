e32a3fdfb82c90e4bde98bdbe89952adeb7d082a
yesmagaly <yesmagaly@gmail.com>
2024-06-07 17:03:08 -0500

    Updated incode credentials

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,5 @@
-export const API_SERVER_URL = "http://adelantto-server.docksal";
-export const VITE_INCODE_API_URL = "https://demo-api.incodesmile.com";
-export const VITE_INCODE_CLIENT_ID = "26mf229";
-export const VITE_INCODE_SDK_URL = "https://sdk.incode.com/sdk/onBoarding-1.57.0.js";
-export const VITE_INCODE_FLOW_ID = "64f1111ac0388f9789122742";
-export const PROD_MODE = false;
+export const API_SERVER_URL = "https://dev-api.adelantto.com";
+export const VITE_INCODE_API_URL = "https://saas-api.incodesmile.com";
+export const VITE_INCODE_CLIENT_ID = "adelantto567";
+export const VITE_INCODE_FLOW_ID = "6663828e59f9d64b9661e2b0";
+export const PROD_MODE = true;
