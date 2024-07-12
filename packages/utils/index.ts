326ff5ab378abff2232232cdf338d6db5df2220c
yesmagaly <yesmagaly@gmail.com>
2024-07-12 00:32:34 -0500

    Updated t helper function

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,12 +1,17 @@
 export const formatCurrency = function (value: number) {
-  return new Intl.NumberFormat('es-MX', { style: 'currency', currency: 'MXN' }).format(value);
-}
+  return new Intl.NumberFormat("es-MX", {
+    style: "currency",
+    currency: "MXN",
+  }).format(value);
+};
 
 export const t = function (text: string) {
   const map = {
     "Your cellphone number is required.": "Tu número de celular es requerido",
     "Your cellphone number is invalid.": "Tu número de celular no es válido",
-  }
+    "Your confirmation code is incorect.":
+      "Su código de confirmación es incorrecto",
+  };
 
   return map[text] ?? text;
-}
+};
