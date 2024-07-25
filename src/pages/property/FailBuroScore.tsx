7f33e70ba629fb3ab7a55ecd9e4c7fcb6ed45508
yesmagaly <yesmagaly@gmail.com>
2024-07-24 21:40:11 -0500

    Updated page

--- src/pages/property/FailBuroScore.tsx
+++ src/pages/property/FailBuroScore.tsx
@@ -10,7 +10,7 @@ const FailBuroScore: React.FC = () => {
     <IonPage>
       <IonContent fullscreen>
         <div className="content heading-light-green">
-          <h1 className="font-bold text-4xl text-center mt-16">¡Lo sentimos!</h1>
+          <h1 className="mt-16 text-center text-4xl font-bold">¡Lo sentimos!</h1>
           <div className="flex items-center">
             <Lottie
               animationData={closeHomeAnimation}
@@ -19,14 +19,14 @@ const FailBuroScore: React.FC = () => {
               play
             />
           </div>
-          <p className="help-text border-y border-solid border-primary-blue py-6 mb-28 text-balance">
+          <p className="help-text mb-28 text-balance border-y border-solid border-primary-blue py-6">
             La puntuación de Buró es baja.<br />
             Te recomendamos intentarlo nuevamente en {" "}
-            <strong>6</strong> meses.
+            <strong>3</strong> meses.
           </p>
 
           <button
-            className="button button is-primary"
+            className="button is-primary"
             onClick={() => router.push("/")}
           >
             Aceptar
