9b1f40090b54a5676d4e3d20541b6849c603178f
yesmagaly <yesmagaly@gmail.com>
2024-08-21 17:40:54 -0500

    Added a new Tag component

---
+++ src/components/Tag.tsx
@@ -0,0 +1,25 @@
+import clsx from 'clsx';
+
+interface ComponentProps {
+  status: "overdue" | "pending" | "approved" | "rejected" | "in_validation";
+  children: React.ReactNode;
+}
+
+export default function Tag({ children, status }: ComponentProps) {
+
+
+  return (
+    <div
+      className={clsx(
+        "text-sm text-white font-semibold rounded py-1 px-6 inline-block",
+        status === 'overdue' && "bg-black",
+        status === 'pending' && "bg-blue-600",
+        status === 'rejected' && "bg-red-700",
+        status === 'approved' && "bg-green-500",
+        status === 'in_validation' && "bg-yellow-500",
+      )}
+    >
+      {children}
+    </div>
+  )
+}
