ddb99cd26009479685b66130382ae65e5f3f581d
yesmagaly <yesmagaly@gmail.com>
2024-10-18 09:03:12 -0500

    TEMP: set development variables

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
-export const API_SERVER_URL = "https://dev-api.adelantto.com";
+export const API_SERVER_URL = "http://adelantto-server.docksal";
 export const VITE_INCODE_API_URL = "https://saas-api.incodesmile.com";
 export const VITE_INCODE_CLIENT_ID = "adelantto567";
 export const VITE_INCODE_FLOW_ID = "6663828e59f9d64b9661e2b0";
-export const PROD_MODE = true;
+export const PROD_MODE = false;
