65e77a5d9a0501c56ebbddf1a9db92858d6bfbbd
yesmagaly <yesmagaly@gmail.com>
2025-01-06 16:30:59 -0500

    Updated configs

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const API_SERVER_URL = "http://adelantto-server.docksal.site";
+export const API_SERVER_URL = "https://dev-api.adelantto.com";
 export const VITE_INCODE_API_URL = "https://saas-api.incodesmile.com";
 export const VITE_INCODE_CLIENT_ID = "adelantto567";
 export const VITE_INCODE_FLOW_ID = "6663828e59f9d64b9661e2b0";
