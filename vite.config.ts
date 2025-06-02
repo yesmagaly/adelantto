161d088cc907e914baf8f613a27a7feade17e142
Joseph Ramos <jardix22@gmail.com>
2025-06-02 14:19:01 -0500

    Updated/removed tailwind files

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
+/// <reference types="vitest" />
+
 import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    legacy()
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-  }
+    legacy(),
+    tailwindcss()
+  ]
 })
