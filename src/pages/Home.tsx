6f2ada6d77e45422a548fad3db34f95e1aebda73
Joseph Ramos <jardix22@gmail.com>
2025-06-10 15:09:26 -0500

    Updated page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,17 @@
 import { IonContent, IonPage, useIonRouter } from "@ionic/react";
 import logo from "../assets/icons/logo.svg";
 
+import adelanttoBgUrl from "../v2/assets/images/adelanto-bg.png"
+
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonContent fullscreen class="ion-padding">
-        <div className="flex h-full flex-col justify-center">
+      <IonContent fullscreen>
+        <div className={`flex flex-col justify-center px-4 py-6 h-full`} >
+          <img src={adelanttoBgUrl} className="absolute inset-0 mx-auto" />
           <div className="mb-14 text-center">
-            <img className="mb-16 inline-block h-40 w-[220px]" src={logo} />
-            <h2 className="text-2xl mb-4 font-semibold">
+            <img className="inline-block mb-16 w-[220px] h-40" src={logo} />
+            <h2 className="mb-4 font-semibold text-2xl">
               Convierte tus rentas{" "}
               <span className="text-emerald-700">en liquidez inmediata</span>
             </h2>
@@ -19,7 +22,7 @@ const Home: React.FC = () => {
             </p>
           </div>
 
-          <div className="grid gap-2">
+          <div className="gap-2 grid">
             <a className="btn btn-primary" href="/login">
               Iniciar sesión
             </a>
