06b384e21a509f7afc59aac9b2b1841aac8f58e6
Joseph Ramos <jardix22@gmail.com>
2025-07-06 11:22:14 -0500

    Updated property pages

--- src/pages/property/DesiredLoan.tsx
+++ src/pages/property/DesiredLoan.tsx
@@ -1,22 +1,27 @@
 import { useState, useEffect } from "react";
-import { IonContent, IonPage, useIonRouter } from "@ionic/react";
+import { IonContent, IonFooter, IonPage, useIonRouter } from "@ionic/react";
 import { RouteComponentProps } from "react-router";
 
 import { applications } from "../../api";
-import * as Page from "../../components/page";
 import { atLeastThreeMonths } from "./LeaseContract";
+import { useForm } from "react-hook-form";
+import {
+  T_application,
+  useLazyGetApplicationQuery,
+  useLazyGetOfferQuery,
+  useUpdateApplicationMutation,
+} from "@adelantto/store";
+import { cn } from "@adelantto/utils";
+
+type T_form = T_application & {
+  desired_loan_term_frame?: number;
+};
 
 function formatCurrency(value: number) {
   return new Intl.NumberFormat("es-MX", {
     style: "currency",
     currency: "MXN",
   }).format(value);
-}
-
-export interface T_application {
-  lease_monthly_income: number;
-  lease_maintenance_fee: number;
-  lease_end_date: string;
 }
 
 interface DesiredLoanProps
@@ -26,99 +31,113 @@ interface DesiredLoanProps
 
 const DesiredLoan: React.FC<DesiredLoanProps> = ({ match }) => {
   const router = useIonRouter();
-  const [error, setError] = useState<string>()!;
-  const [loading, setLoading] = useState(true);
-  const [months, setMonths] = useState<number>()!;
-  const [application, setApplication] = useState<T_application>();
+  const [trigger, { data: application, isLoading }] =
+    useLazyGetApplicationQuery();
+  const [mutation] = useUpdateApplicationMutation();
+  const [getLazyOffer, { data: offer }] = useLazyGetOfferQuery();
+  const [options, setOptions] = useState<Array<number>>([]);
 
-  // GET with fetch API
+  const { register, setValue, handleSubmit, watch } = useForm<T_form>({
+    defaultValues: async () => await trigger(match.params.id).unwrap(),
+  });
+
+  const desired_loan_term_frame = watch("desired_loan_term_frame");
+
   useEffect(() => {
-    const fetchData = async () => {
-      const response = await applications.get(match.params.id);
-      const data = await response.json();
+    if (application?.lease_end_date) {
+      setOptions(
+        [3, 6, 8, 12].filter((value) =>
+          atLeastThreeMonths(application.lease_end_date, value)
+        )
+      );
+    }
+  }, [application]);
 
-      setLoading(false);
-      setApplication(data);
+  useEffect(() => {
+    const fetchLoanContract = async () => {
+      if (desired_loan_term_frame && application?.lease_monthly_income) {
+        const data = await getLazyOffer({
+          principal: application.lease_monthly_income * desired_loan_term_frame,
+          months: desired_loan_term_frame,
+        }).unwrap();
+
+        setValue("pre_offer_amount", data.principal);
+        setValue("pre_offer_fees", data.fees);
+        setValue("pre_offer_commissions", data.iva_commission);
+        setValue("pre_offer_term_frame", months);
+      }
     };
+    fetchLoanContract();
+  }, [desired_loan_term_frame]);
 
-    fetchData();
-  }, []);
-
-  const handleOptionClick = (value: number) => () => {
-    setMonths(value);
-  };
-
-  const handleClick = async () => {
-    if (months) {
-      await applications.desiredLoan(match.params.id, {
-        desired_loan_amount: application.lease_monthly_income * months,
-        desired_loan_term_frame: months,
-
-        step: "desired_loan",
-      });
+  const onSubmit = async (form: T_form) => {
+    if (application?.lease_monthly_income && desired_loan_term_frame) {
+      await mutation({
+        ...form,
+        desired_loan_amount:
+          application.lease_monthly_income * desired_loan_term_frame,
+      }).unwrap();
 
       router.push(`/applications/${match.params.id}/pre-offer`);
-    } else {
-      setError("Por favor, selecciona un monto para continuar");
     }
   };
 
   return (
     <IonPage>
-      <IonContent fullscreen>
-        <Page.Root>
-          <Page.Header>
-            <div className="heading heading--blue">
-              <div className="heading__pager text-right">Paso 2 de 7</div>
-              <h1 className="heading-3">
-                Selecciona el monto <br />
-                <strong>que te gustaría recibir</strong>
-              </h1>
-            </div>
-          </Page.Header>
-          <Page.Body className="flex flex-col gap-2">
-            {loading && <div>Loading</div>}
-
-            {!loading &&
+      <IonContent className="ion-padding">
+        <form id="form" onSubmit={handleSubmit(onSubmit)}>
+          <h3 className="text-h6 text-center">Cantidad de meses</h3>
+          <div className="flex gap-4">
+            {!isLoading &&
               application &&
-              [6, 9, 12]
-                .filter((value) =>
-                  atLeastThreeMonths(application?.lease_end_date, value)
-                )
-                .map((value) => (
-                  <button
-                    key={`dl-${value}`}
-                    id={`desired-loan-${value}`}
-                    className={`button ${
-                      value === months ? "is-secondary" : ""
-                    }`}
-                    onClick={handleOptionClick(value)}
-                  >
-                    <div className="w-full text-2xl">
-                      {formatCurrency(
-                        value *
-                          (application.lease_monthly_income -
-                            application.lease_maintenance_fee)
-                      )}
-                    </div>
-                    <div className="font-normal">x {value} meses</div>
-                  </button>
-                ))}
-
-            {error && !months && (
-              <p className="mt-4 text-center font-medium text-orange-500">
-                {error}
-              </p>
-            )}
-          </Page.Body>
-
-          <Page.Footer>
-            <button className="button is-primary" onClick={handleClick}>
-              Siguiente
-            </button>
-          </Page.Footer>
-        </Page.Root>
+              options.map((value) => (
+                <button
+                  key={`dl-${value}`}
+                  id={`desired-loan-${value}`}
+                  type="button"
+                  className={cn(
+                    "btn btn-outline flex-1/4 h-30 rounded-xl",
+                    value === desired_loan_term_frame && "btn-active"
+                  )}
+                  onClick={() => setValue("desired_loan_term_frame", value)}
+                >
+                  <div className="w-full text-2xl">{value}</div>
+                </button>
+              ))}
+          </div>
+          Definir manualmente
+          {offer && (
+            <div className="bg-blue-600 card">
+              <div className="gap-4 card-body">
+                <div className="text-white text-center">
+                  <div className="text-emerald-300 text-h2">
+                    {formatCurrency(offer.advance)} MXN
+                  </div>
+                  <p className="text-purple-200">Total que recibiras</p>
+                </div>
+                <div className="text-white text-center">
+                  <div className="text-h4">
+                    {formatCurrency(offer.principal)} MXN
+                  </div>
+                  <p className="text-purple-200">Total que pagarás</p>
+                </div>
+                <div className="text-white text-center">
+                  <div className="text-h4">
+                    {formatCurrency(offer.fees)} MXN
+                  </div>
+                  <p className="text-purple-200">MXN Pago de seguro</p>
+                </div>
+              </div>
+            </div>
+          )}
+        </form>
       </IonContent>
+
+      <IonFooter className="ion-padding">
+        <button type="submit" form="form" className="btn-block btn btn-primary">
+          Subir documentación
+        </button>
+      </IonFooter>
     </IonPage>
   );
 };
