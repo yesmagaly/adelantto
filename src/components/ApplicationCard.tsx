81979eeb2571e42608c9e4818ec8558b4345098a
Joseph Ramos <jardix22@gmail.com>
2025-07-10 14:54:01 -0500

    Updated application card

--- src/components/ApplicationCard.tsx
+++ src/components/ApplicationCard.tsx
@@ -29,24 +29,45 @@ export default function ApplicationCard({
   return (
     <div className={`card card-sm bg-gray-200 ${className}`}>
       <div className="card-body">
-        {item.status === "uncompleted" && (
-          <>
-            <div className="text-sm">Renta mensual:</div>
-            <h3 className="mb-2 text-xl heading-5">
-              {formatCurrency(item.lease_monthly_income)}{" "}
-              <span className="text-xs">{item.status} | {item.step}</span>
-            </h3>
+        <h4 className="font-semibold text-lg leading-5">
+          Solicitud AdelanttoCash® {item.id.toString().padStart(4, "S0")}
+        </h4>
+        <dl>
+          {item.status === "uncompleted" && (
+            <>
+              <dt className="float-left mr-1 mb-1">Renta mensual:</dt>
+              <dd className="mb-1 font-bold">
+                {formatCurrency(item.lease_monthly_income)} MXN
+              </dd>
 
-            {redirectTo && (
-              <button
-                className="btn btn-sm"
-                onClick={() => router.push(redirectTo)}
-              >
-                Continuar
-              </button>
-            )}
-          </>
-        )}
+              <dt className="float-left mr-1 mb-1">Pre-oferta Monto:</dt>
+              <dd className="mb-1 font-bold">
+                {formatCurrency(item.pre_offer_amount)} MXN
+              </dd>
+
+
+              {/* pre_offer_commissions
+              pre_offer_fees
+              pre_offer_term_frame */}
+
+              <div className="text-sm"></div>
+              <h3 className="mb-2 text-xl heading-5">
+                <span className="text-xs">
+                  {item.status} | {item.step}
+                </span>
+              </h3>
+
+              {redirectTo && (
+                <button
+                  className="btn btn-sm"
+                  onClick={() => router.push(redirectTo)}
+                >
+                  Continuar
+                </button>
+              )}
+            </>
+          )}
+        </dl>
 
         {item.status !== "uncompleted" && (
           <>
