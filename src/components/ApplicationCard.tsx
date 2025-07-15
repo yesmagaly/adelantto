cf24b595d3adf2f04c557aa5639d5508a9f2ea56
Joseph Ramos <jardix22@gmail.com>
2025-07-15 10:41:57 -0500

    Updated components

--- src/components/ApplicationCard.tsx
+++ src/components/ApplicationCard.tsx
@@ -1,14 +1,13 @@
 import { useIonRouter } from "@ionic/react";
 import { formatCurrency } from "@adelantto/utils";
 
-import { nextStepUrl } from "../utils/steps";
-import { ApplicationType } from "../types";
+import { applicationStepsUrls, T_application } from "@adelantto/store";
 
 export const trans = function (key: string) {
   const mapper: { [key: string]: string } = {
     uncompleted: "Borrador",
     awaiting_validation: "Esperando validación",
-    validating: "Validando",
+    in_validation: "En validación",
     approved: "Aceptado",
     rejected: "Rechazado",
   };
@@ -20,33 +19,34 @@ export default function ApplicationCard({
   item,
   className,
 }: {
-  item: ApplicationType;
+  item: T_application;
   className: string;
 }) {
   const router = useIonRouter();
-  const redirectTo = nextStepUrl(item);
+
+  const redirectTo =
+    item?.next_step &&
+    `/applications/${item.id}${applicationStepsUrls[item.next_step]}`;
 
   return (
     <div className={`card card-sm bg-gray-200 ${className}`}>
       <div className="card-body">
+        <div className="badge-outline text-xs badge badge-sm">
+          {trans(item.status)}
+        </div>
+
         <h4 className="font-semibold text-lg leading-4">
           Solicitud AdelanttoCash® {item.id.toString().padStart(4, "S0")}
         </h4>
-        <div className="flex gap-2 mb-2">
-          <div className="badge-outline text-xs badge">{item.step}</div>
-          <div className="badge-outline text-xs badge">{item.status}</div>
-        </div>
 
         <dl>
-          {item.status === "uncompleted" && (
-            <>
-              <dt className="float-left mr-1 mb-1">
-                Valor de la renta mensual:
-              </dt>
-              <dd className="mb-1 font-bold">
-                {formatCurrency(item.lease_monthly_income)} MXN
-              </dd>
+          <dt className="float-left mr-1 mb-1">Valor de la renta mensual:</dt>
+          <dd className="mb-1 font-bold">
+            {formatCurrency(item.lease_monthly_income)} MXN
+          </dd>
 
+          {item?.pre_offer_term_frame && (
+            <>
               <dt className="mt-2 font-semibold">Pre-oferta</dt>
               <dd className="mb-1 font-bold"></dd>
 
@@ -72,30 +72,17 @@ export default function ApplicationCard({
               <dd className="mb-1 font-bold">
                 {formatCurrency(item.pre_offer_fees)} MXN
               </dd>
-
-              {redirectTo && (
-                <button
-                  className="btn-block mt-2 btn btn-primary"
-                  onClick={() => router.push(redirectTo)}
-                >
-                  Continuar
-                </button>
-              )}
             </>
           )}
         </dl>
 
-        {item.status !== "uncompleted" && (
-          <>
-            <div className="text-sm">Adelanto:</div>
-            <h3 className="text-xl heading-5">
-              {formatCurrency(item.pre_offer_amount)}
-            </h3>
-            <div className="mb-4 text-base">
-              x {item.pre_offer_term_frame} meses
-            </div>
-            <div>{item.status !== "approved" && trans(item.status)}</div>
-          </>
+        {item.status !== "in_validation" && (
+          <button
+            className="btn-block mt-2 btn btn-primary"
+            onClick={() => router.push(redirectTo)}
+          >
+            Continuar
+          </button>
         )}
       </div>
     </div>
--- src/components/LoanCard.tsx
+++ src/components/LoanCard.tsx
@@ -4,12 +4,14 @@ import { es } from "date-fns/locale";
 
 import { T_loan_item } from "@adelantto/store";
 import { formatCurrency } from "@adelantto/utils";
+import { MaterialIcon } from "@adelantto/core";
 
 function trans(key: string) {
   const mapper = {
-    awaiting_account_statement_upload: "Esperando la carga de estado de cuenta",
+    awaiting_account_statement_upload:
+      "Esperando la carga de tu estado de cuenta",
     awaiting_account_statement_check:
-      "Su estado de cuenta esta a la espera de revisión",
+      "Tu estado de cuenta bancaria esta en revisión",
     awaiting_client_signature:
       "Estamos esperamos su firma digital del contrato. Por favor revise el correo electrónico de Weetrust.",
     signature_document_completed: "Firma de documento completado",
@@ -38,48 +40,51 @@ export default function LoanCard({
   console.log({ amount, status, id, ...loan });
 
   return (
-    <div className="bg-linear-to-r from-indigo-600 to-indigo-300 text-white card">
+    <div className="bg-linear-to-r from-indigo-600 to-indigo-300 text-white card card-sm">
       <div className="card-body">
         <h4 className="font-semibold text-lg leading-5">
           AdelanttoCash® {id.toString().padStart(4, "0")}
         </h4>
 
-        <dl>
-          {loan.missing_installment && (
-            <>
-              <dt className="mr-1">Valor a pagar este mes</dt>
-              <dd className="mb-2 font-bold text-emerald-300 text-h3">
-                {formatCurrency(loan.missing_installment?.amount)} MXN
-              </dd>
+        {status === "active" && (
+          <dl>
+            {loan.missing_installment && (
+              <>
+                <dt className="mr-1">Valor a pagar este mes</dt>
+                <dd className="mb-2 font-bold text-emerald-300 text-h3">
+                  {formatCurrency(loan.missing_installment?.amount)} MXN
+                </dd>
 
-              <dt className="float-left mr-1 mb-1">Fecha límite:</dt>
-              <dd className="mb-1 font-bold">
-                {format(loan.missing_installment?.due_date, "d 'de' MMMM", {
-                  locale: es,
-                })}
-              </dd>
-            </>
-          )}
+                <dt className="float-left mr-1 mb-1">Fecha límite:</dt>
+                <dd className="mb-1 font-bold">
+                  {format(loan.missing_installment?.due_date, "d 'de' MMMM", {
+                    locale: es,
+                  })}
+                </dd>
+              </>
+            )}
 
-          <dt className="float-left mr-1 mb-1">Total pagado:</dt>
-          <dd className="mb-1 font-bold">{formatCurrency(paid_amount)} MXN</dd>
+            <dt className="float-left mr-1 mb-1">Total pagado:</dt>
+            <dd className="mb-1 font-bold">
+              {formatCurrency(paid_amount)} MXN
+            </dd>
 
-          <dt className="float-left mr-1 mb-1">Deuda total:</dt>
-          <dd className="mb-1 font-bold">{formatCurrency(amount)} MXN</dd>
+            <dt className="float-left mr-1 mb-1">Deuda total:</dt>
+            <dd className="mb-1 font-bold">{formatCurrency(amount)} MXN</dd>
 
-          <dt className="float-left mr-1">Cuotas restantes:</dt>
-          <dd className="font-bold">{missing_installments_counter}</dd>
-        </dl>
+            <dt className="float-left mr-1">Cuotas restantes:</dt>
+            <dd className="font-bold">{missing_installments_counter}</dd>
+          </dl>
+        )}
 
-        {/* {status === "awaiting_account_statement_upload" &&
-          application.status === "approved" && (
-            <button
-              className="bg-green-500 px-3 py-1 rounded-sm font-regular text-white leading-5"
-              onClick={() => router.push(`/loans/${id}/success`)}
-            >
-              Continuar
-            </button>
-          )} */}
+        {status === "awaiting_account_statement_upload" && (
+          <button
+            className="mt-4 btn btn-primary"
+            onClick={() => router.push(`/loans/${id}/success`)}
+          >
+            Continuar
+          </button>
+        )}
 
         {status === "active" && (
           <button className="btn" onClick={showMore}>
@@ -88,8 +93,11 @@ export default function LoanCard({
         )}
 
         {status !== "active" && (
-          <div className="pt-4">
-            <p className="leading-tight">{trans(status)}</p>
+          <div className="flex gap-1.5 mt-2">
+            <MaterialIcon name="info" size="18px" />
+            <p className="text-sm leading-4">
+              {trans(status)}
+            </p>
           </div>
         )}
       </div>
