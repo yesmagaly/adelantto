295d6d731f1a36379cd4cf4759027afd20ab9174
Joseph Ramos <jardix22@gmail.com>
2025-07-23 18:55:45 -0500

    Set production parameters

--- packages/store/src/constants.ts
+++ packages/store/src/constants.ts
@@ -1,2 +1,2 @@
-// export const BASE_URL = 'https://dev-api.adelanttocash.com';
-export const BASE_URL = 'http://adelantto-server.docksal.site';
+export const BASE_URL = 'https://dev-api.adelanttocash.com';
+// export const BASE_URL = 'http://adelantto-server.docksal.site';
